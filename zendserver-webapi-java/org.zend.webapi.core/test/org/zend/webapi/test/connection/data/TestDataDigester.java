1532c55b578f30cd0a9997857d0a74ef8f8f6f3b
Wojciech Galanciak <wojciech.galanciak@gmail.com>
2011-04-18 11:58:06 +0000

    Improve ApplicationInfo parsing test.

--- zendserver-webapi-java/org.zend.webapi.core/test/org/zend/webapi/test/connection/data/TestDataDigester.java
+++ zendserver-webapi-java/org.zend.webapi.core/test/org/zend/webapi/test/connection/data/TestDataDigester.java
@@ -8,17 +8,23 @@
 package org.zend.webapi.test.connection.data;
 
 import java.io.IOException;
+import java.util.List;
 
 import org.junit.Assert;
 import org.junit.Test;
 import org.restlet.ext.xml.DomRepresentation;
 import org.zend.webapi.core.connection.data.ApplicationInfo;
+import org.zend.webapi.core.connection.data.ApplicationServer;
+import org.zend.webapi.core.connection.data.ApplicationServers;
 import org.zend.webapi.core.connection.data.ApplicationsList;
 import org.zend.webapi.core.connection.data.DataDigster;
+import org.zend.webapi.core.connection.data.DeployedVersion;
 import org.zend.webapi.core.connection.data.IResponseData;
+import org.zend.webapi.core.connection.data.MessageList;
 import org.zend.webapi.core.connection.data.ServerInfo;
 import org.zend.webapi.core.connection.data.ServersList;
 import org.zend.webapi.core.connection.data.SystemInfo;
+import org.zend.webapi.core.connection.data.values.ApplicationStatus;
 import org.zend.webapi.core.connection.data.values.LicenseInfoStatus;
 import org.zend.webapi.core.connection.data.values.SystemEdition;
 import org.zend.webapi.core.connection.data.values.WebApiVersion;
@@ -170,6 +176,70 @@ public class TestDataDigester {
 				"applicationRedeploy.xml",
 				IResponseData.ResponseType.APPLICATIONS_LIST);
 		Assert.assertNotNull(responseData.getApplicationsInfo().size() > 0);
+	}
+
+	@Test
+	public void testApplicationInfo() throws Exception {
+		final ApplicationsList responseData = (ApplicationsList) getResponseData(
+				"applicationGetStatus.xml",
+				IResponseData.ResponseType.APPLICATIONS_LIST);
+		List<ApplicationInfo> applicationInfo = responseData
+				.getApplicationsInfo();
+		Assert.assertNotNull(applicationInfo.size() == 2);
+		ApplicationInfo firstApp = applicationInfo.get(0);
+		Assert.assertSame(1, firstApp.getId());
+		Assert.assertTrue("My Application".equals(firstApp.getAppName()));
+		Assert.assertTrue("User Name".equals(firstApp.getUserAppName()));
+		Assert.assertTrue("/usr/local/aaa".equals(firstApp
+				.getInstalledLocation()));
+		Assert.assertSame(ApplicationStatus.OK, firstApp.getStatus());
+
+		ApplicationServers servers = firstApp.getServers();
+		Assert.assertNotNull(servers);
+		List<ApplicationServer> serversList = servers.getApplicationServers();
+		Assert.assertTrue(serversList.size() == 2);
+		ApplicationServer firstServer = serversList.get(0);
+		Assert.assertTrue("2.14.0".equals(firstServer.getDeployedVersion()));
+		ApplicationServer secondServer = serversList.get(1);
+		Assert.assertTrue("2.15.0".equals(secondServer.getDeployedVersion()));
+
+		List<DeployedVersion> versions = firstApp.getDeployedVersions()
+				.getDeployedVersions();
+		Assert.assertTrue(versions.size() == 3);
+		Assert.assertTrue("1.4".equals(versions.get(0).getVersion()));
+		Assert.assertTrue("1.5".equals(versions.get(1).getVersion()));
+		Assert.assertTrue("1.6".equals(versions.get(2).getVersion()));
+
+		MessageList messageList = firstApp.getMessageList();
+		Assert.assertNotNull(messageList);
+		Assert.assertTrue("test error".equals(messageList.getError().get(0)));
+
+		ApplicationInfo secondApp = applicationInfo.get(1);
+		Assert.assertSame(2, secondApp.getId());
+		Assert.assertTrue("Blog 2.0".equals(secondApp.getAppName()));
+		Assert.assertTrue("User Name".equals(secondApp.getUserAppName()));
+		Assert.assertTrue("/usr/local/aaa".equals(secondApp
+				.getInstalledLocation()));
+		Assert.assertSame(ApplicationStatus.UPLOADED, secondApp.getStatus());
+
+		servers = secondApp.getServers();
+		Assert.assertNotNull(servers);
+		serversList = servers.getApplicationServers();
+		Assert.assertTrue(serversList.size() == 1);
+		Assert.assertTrue("2.1.0".equals(serversList.get(0)
+				.getDeployedVersion()));
+
+		versions = secondApp.getDeployedVersions().getDeployedVersions();
+		Assert.assertTrue(versions.size() == 1);
+		Assert.assertTrue("1.6".equals(versions.get(0).getVersion()));
+
+		messageList = secondApp.getMessageList();
+		Assert.assertNotNull(messageList);
+		Assert.assertTrue("test second error".equals(messageList.getError()
+				.get(0)));
+		Assert.assertTrue("test info".equals(messageList.getInfo().get(0)));
+		Assert.assertTrue("test warning"
+				.equals(messageList.getWarning().get(0)));
 	}
 
 }
