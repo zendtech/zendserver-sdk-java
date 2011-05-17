e21fa27149a10ed195852a461b321890e1e5e232
wgalanciak <wojciech.galanciak@gmail.com>
2011-05-17 21:38:14 +0000

    Add CommandTypes.

---
+++ zendserver-sdk-java/org.zend.sdk/sdkcli/org/zend/sdkcli/CommandFactory.java
@@ -0,0 +1,48 @@
+/*******************************************************************************
+ * Copyright (c) May 17, 2011 Zend Technologies Ltd. 
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Eclipse Public License v1.0 
+ * which accompanies this distribution, and is available at 
+ * http://www.eclipse.org/legal/epl-v10.html  
+ *******************************************************************************/
+
+package org.zend.sdkcli;
+
+import org.zend.sdkcli.internal.commands.CommandLine;
+import org.zend.sdkcli.internal.commands.CreateProjectCommand;
+
+/**
+ * Creates command instance.
+ * 
+ * @author Wojciech Galanciak, 2011
+ * 
+ */
+public class CommandFactory {
+
+	/**
+	 * Creates {@link ICommand} implementor instance based on a given command
+	 * line.
+	 * 
+	 * @param line
+	 *            - command line
+	 * @return command instance
+	 * @throws ParseError
+	 */
+	public static ICommand createCommand(CommandLine line) throws ParseError {
+		CommandTypes type = CommandTypes.byCommandLine(line);
+		ICommand command = null;
+		switch (type) {
+		case CREATE_PROJECT:
+			command = new CreateProjectCommand(line);
+			break;
+		case UPDATE_PROJECT:
+			break;
+		case DELETE_PROJECT:
+			break;
+		default:
+			break;
+		}
+		return command;
+	}
+
+}
