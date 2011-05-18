11340c34cae765ce225c4c8c2149246d9e233f90
wgalanciak <wojciech.galanciak@gmail.com>
2011-05-18 12:33:09 +0000

    Refactoring

--- org.zend.sdk/test/org/zend/sdk/test/sdkcli/TestCommandTypes.java
+++ org.zend.sdk/test/org/zend/sdk/test/sdkcli/TestCommandTypes.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.assertNotSame;
 import static org.junit.Assert.assertSame;
 
 import org.junit.Test;
-import org.zend.sdkcli.CommandTypes;
+import org.zend.sdkcli.CommandType;
 import org.zend.sdkcli.ParseError;
 import org.zend.sdkcli.internal.commands.CommandLine;
 
@@ -14,9 +14,9 @@ public class TestCommandTypes {
 	@Test
 	public void testValidCommandType() throws ParseError {
 		CommandLine line = new CommandLine(new String[] { "create", "project" });
-		CommandTypes type = CommandTypes.byCommandLine(line);
+		CommandType type = CommandType.byCommandLine(line);
 		assertNotNull(type);
-		assertNotSame(CommandTypes.UNKNOWN, type);
+		assertNotSame(CommandType.UNKNOWN, type);
 		assertSame("create", type.getVerb());
 		assertSame("project", type.getDirectObject());
 	}
@@ -24,25 +24,25 @@ public class TestCommandTypes {
 	@Test
 	public void testInvalidCommandType() throws ParseError {
 		CommandLine line = new CommandLine(new String[] { "random", "123" });
-		CommandTypes type = CommandTypes.byCommandLine(line);
+		CommandType type = CommandType.byCommandLine(line);
 		assertNotNull(type);
-		assertSame(CommandTypes.UNKNOWN, type);
+		assertSame(CommandType.UNKNOWN, type);
 	}
 
 	@Test
 	public void testInvalidCommandType2() throws ParseError {
 		CommandLine line = new CommandLine(new String[] { "random" });
-		CommandTypes type = CommandTypes.byCommandLine(line);
+		CommandType type = CommandType.byCommandLine(line);
 		assertNotNull(type);
-		assertSame(CommandTypes.UNKNOWN, type);
+		assertSame(CommandType.UNKNOWN, type);
 	}
 
 	@Test
 	public void testInvalidCommandType3() throws ParseError {
 		CommandLine line = new CommandLine(new String[] {});
-		CommandTypes type = CommandTypes.byCommandLine(line);
+		CommandType type = CommandType.byCommandLine(line);
 		assertNotNull(type);
-		assertSame(CommandTypes.UNKNOWN, type);
+		assertSame(CommandType.UNKNOWN, type);
 	}
 
 }
