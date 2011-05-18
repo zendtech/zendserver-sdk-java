9cb12f9a0fba4e2d67cec96e28824419e82f3928
wgalanciak <wojciech.galanciak@gmail.com>
2011-05-18 12:11:15 +0000

    Add listeners for process monitoring.

---
+++ zendserver-sdk-java/org.zend.sdk/sdklib/org/zend/sdklib/library/IStatus.java
@@ -0,0 +1,34 @@
+/*******************************************************************************
+ * Copyright (c) May 18, 2011 Zend Technologies Ltd. 
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Eclipse Public License v1.0 
+ * which accompanies this distribution, and is available at 
+ * http://www.eclipse.org/legal/epl-v10.html  
+ *******************************************************************************/
+
+package org.zend.sdklib.library;
+
+/**
+ * Represents status of a library process.
+ * 
+ * @author Wojciech Galanciak, 2011
+ * 
+ */
+public interface IStatus {
+
+	/**
+	 * @return status code of a process
+	 */
+	StatusCode getCode();
+
+	/**
+	 * @return title of a status; can be <code>null</code>
+	 */
+	String getTitle();
+
+	/**
+	 * @return message related to a status; can be <code>null</code>
+	 */
+	String getMessage();
+
+}
