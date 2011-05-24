825812c4b78cdaa3306d09076e7b0fc6cba3e7e4
jacek.pospychala@gmail.com <jacek.pospychala@gmail.com@b75f36dc-5d77-e969-e1c6-c1f8c8b91a4e>
2011-05-24 14:12:12 +0000

    Issue 15:	[lib] Add create method to ZendProject

--- org.zend.sdk/test/org/zend/sdk/test/sdklib/library/TestZendProject.java
+++ org.zend.sdk/test/org/zend/sdk/test/sdklib/library/TestZendProject.java
@@ -1,7 +1,9 @@
 package org.zend.sdk.test.sdklib.library;
 
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+
+import java.io.File;
 
 import org.junit.Test;
 import org.zend.sdk.test.AbstractTest;
@@ -12,30 +14,55 @@ public class TestZendProject extends AbstractTest {
 
 	@Test
 	public void testZendProjectCreation1() throws ParseError {
-		ZendProject project = new ZendProject("name", "target", "index", "path");
-		assertNotNull(project);
+		new File("testdir1").mkdir();
+		ZendProject project = new ZendProject("name", true, "testdir1");
 		assertTrue(project.create());
+		
+		assertTrue(new File("testdir1/descriptor.xml").exists());
+		assertTrue(new File("testdir1/public/index.html").exists());
+		assertTrue(new File("testdir1/scripts/post_activate.php").exists());
+		assertTrue(new File("testdir1/scripts/post_deactivate.php").exists());
+		assertTrue(new File("testdir1/scripts/post_stage.php").exists());
+		assertTrue(new File("testdir1/scripts/post_unstage.php").exists());
+		assertTrue(new File("testdir1/scripts/pre_activate.php").exists());
+		assertTrue(new File("testdir1/scripts/pre_deactivate.php").exists());
+		assertTrue(new File("testdir1/scripts/pre_stage.php").exists());
+		assertTrue(new File("testdir1/scripts/pre_unstage.php").exists());
 	}
 
 	@Test
 	public void testZendProjectCreation2() throws ParseError {
-		ZendProject project = new ZendProject("name", "target", "index", null);
-		assertNotNull(project);
+		ZendProject project = new ZendProject("name", true, null);
 		assertTrue(project.create());
+		
+		assertTrue(new File("descriptor.xml").exists());
+		assertTrue(new File("public/index.html").exists());
+		assertTrue(new File("scripts/post_activate.php").exists());
+		assertTrue(new File("scripts/post_deactivate.php").exists());
+		assertTrue(new File("scripts/post_stage.php").exists());
+		assertTrue(new File("scripts/post_unstage.php").exists());
+		assertTrue(new File("scripts/pre_activate.php").exists());
+		assertTrue(new File("scripts/pre_deactivate.php").exists());
+		assertTrue(new File("scripts/pre_stage.php").exists());
+		assertTrue(new File("scripts/pre_unstage.php").exists());
 	}
 
 	@Test
 	public void testZendProjectCreation3() throws ParseError {
-		ZendProject project = new ZendProject("name", "target", null, null);
-		assertNotNull(project);
+		new File("testdir3").mkdir();
+		ZendProject project = new ZendProject("name", false, "testdir3");
 		assertTrue(project.create());
-	}
-
-	@Test
-	public void testZendProjectCreation4() throws ParseError {
-		ZendProject project = new ZendProject("name", null, null, null);
-		assertNotNull(project);
-		assertTrue(project.create());
+		
+		assertTrue(new File("testdir3/descriptor.xml").exists());
+		assertTrue(new File("testdir3/public/index.html").exists());
+		assertFalse(new File("testdir3/scripts/post_activate.php").exists());
+		assertFalse(new File("testdir3/scripts/post_deactivate.php").exists());
+		assertFalse(new File("testdir3/scripts/post_stage.php").exists());
+		assertFalse(new File("testdir3/scripts/post_unstage.php").exists());
+		assertFalse(new File("testdir3/scripts/pre_activate.php").exists());
+		assertFalse(new File("testdir3/scripts/pre_deactivate.php").exists());
+		assertFalse(new File("testdir3/scripts/pre_stage.php").exists());
+		assertFalse(new File("testdir3/scripts/pre_unstage.php").exists());
 	}
 
 }
