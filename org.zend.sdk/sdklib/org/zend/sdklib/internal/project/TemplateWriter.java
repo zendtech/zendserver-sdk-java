bcf1f624f93b066d18b6e15ca21c83b15e08dec8
jacek.pospychala@gmail.com <jacek.pospychala@gmail.com@b75f36dc-5d77-e969-e1c6-c1f8c8b91a4e>
2011-05-25 14:29:54 +0000

    Issue 18:	[cli] Add update project command

--- org.zend.sdk/sdklib/org/zend/sdklib/internal/project/TemplateWriter.java
+++ org.zend.sdk/sdklib/org/zend/sdklib/internal/project/TemplateWriter.java
@@ -56,6 +56,9 @@ public class TemplateWriter {
 	}
 
 	private void writeDescriptor(String name, boolean withScripts, Writer out) throws IOException {
+		if (name == null) {
+			throw new IllegalArgumentException("Name parameter is required in order to create descriptor");
+		}
 		out.append("<?xml version=\"1.0\" encoding=\"utf-8\"?>\n");
 		out.append("<package version=\"1.4.11\" xmlns=\"http://www.zend.com/server/deployment-descriptor/1.0\" xmlns:xsi=\">http://www.w3.org/2001/XMLSchema-instance\">\n");
 		out.append(" <name>").append(xmlEscape(name)).append("</name>\n");
