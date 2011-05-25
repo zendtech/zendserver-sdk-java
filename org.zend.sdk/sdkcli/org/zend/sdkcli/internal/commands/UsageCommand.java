20895b0929615cda09cb7557bef6e741812a97f8
jacek.pospychala@gmail.com <jacek.pospychala@gmail.com@b75f36dc-5d77-e969-e1c6-c1f8c8b91a4e>
2011-05-25 15:45:59 +0000

    Issue 7:	[cli] Create help command

--- org.zend.sdk/sdkcli/org/zend/sdkcli/internal/commands/UsageCommand.java
+++ org.zend.sdk/sdkcli/org/zend/sdkcli/internal/commands/UsageCommand.java
@@ -2,6 +2,7 @@ package org.zend.sdkcli.internal.commands;
 
 import java.util.Collection;
 
+import org.apache.commons.cli.Option;
 import org.zend.sdkcli.CommandFactory;
 import org.zend.sdkcli.CommandType;
 import org.zend.sdkcli.ICommand;
@@ -14,13 +15,18 @@ public class UsageCommand implements ICommand {
 		for (CommandType type : CommandType.values()) {
 			ICommand cmd = CommandFactory.createCommand(type);
 
-			System.out.println(type);
+			String dirObj = type.getDirectObject();
+			if (dirObj == null) {
+				dirObj = "";
+			}
+			System.out.println("zend "+type.getVerb() +" "+dirObj+" [options] - "+type.getInfo());
 			CommandOptions opts = cmd.getOptions();
 			if (opts != null) {
-				System.out.println("  Options:");
+				System.out.println(" Options:");
 				Collection collection = opts.getOptions();
 				for (Object o : collection) {
-					System.out.println("    " + o);
+					Option opt = (Option) o;
+					System.out.printf("  -%-20s %s\n", opt.getOpt(), opt.getDescription());
 				}
 			}
 
