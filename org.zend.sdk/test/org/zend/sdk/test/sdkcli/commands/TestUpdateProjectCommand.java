3ecd92e6265db7450b5d46f7f6d48b3912219793
jacek.pospychala@gmail.com <jacek.pospychala@gmail.com@b75f36dc-5d77-e969-e1c6-c1f8c8b91a4e>
2011-05-25 14:16:20 +0000

    Issue 18:	[cli] Add update project command

---
+++ org.zend.sdk/test/org/zend/sdk/test/sdkcli/commands/TestUpdateProjectCommand.java
@@ -0,0 +1,54 @@
+package org.zend.sdk.test.sdkcli.commands;
+
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+import org.zend.sdk.test.AbstractTest;
+import org.zend.sdkcli.CommandFactory;
+import org.zend.sdkcli.ICommand;
+import org.zend.sdkcli.ParseError;
+import org.zend.sdkcli.internal.commands.CommandLine;
+import org.zend.sdkcli.internal.commands.UpdateProjectCommand;
+
+public class TestUpdateProjectCommand extends AbstractTest {
+
+	@Test
+	public void testByCommandFactory() throws ParseError {
+		CommandLine cmdLine = new CommandLine(new String[] { "update",
+				"project", "-name", "testName" });
+		ICommand command = CommandFactory.createCommand(cmdLine);
+		assertNotNull(command);
+		assertTrue(command.execute());
+	}
+
+	@Test
+	public void testByConstructor1() throws ParseError {
+		CommandLine cmdLine = new CommandLine(new String[] { "update",
+				"project", "-name", "testName", "-destination", "abc" });
+		ICommand command = new UpdateProjectCommand(cmdLine);
+		assertNotNull(command);
+		assertTrue(command.execute());
+	}
+	
+	@Test
+	public void testByConstructor2() throws ParseError {
+		CommandLine cmdLine = new CommandLine(new String[] { "update",
+				"project", "-name", "testName", "-destination", "def", "-no_scripts" });
+		ICommand command = new UpdateProjectCommand(cmdLine);
+		assertNotNull(command);
+		assertTrue(command.execute());
+	}
+	
+	@Test
+	public void testByConstructor3() throws ParseError {
+		CommandLine cmdLine = new CommandLine(new String[] { "update",
+				"-destination", "ghi" });
+		ICommand command = new UpdateProjectCommand(cmdLine);
+		assertNotNull(command);
+		
+		assertFalse(command.execute());
+	}
+}
