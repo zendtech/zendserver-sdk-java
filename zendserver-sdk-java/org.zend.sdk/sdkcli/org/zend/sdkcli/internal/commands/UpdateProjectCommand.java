d1110620aed6f949ddab2aa9e84a2477b86c18dc
jacek.pospychala@gmail.com <jacek.pospychala@gmail.com@b75f36dc-5d77-e969-e1c6-c1f8c8b91a4e>
2011-05-25 14:29:06 +0000

    Issue 18:	[cli] Add update project command

---
+++ zendserver-sdk-java/org.zend.sdk/sdkcli/org/zend/sdkcli/internal/commands/UpdateProjectCommand.java
@@ -0,0 +1,89 @@
+/*******************************************************************************
+ * Copyright (c) May 16, 2011 Zend Technologies Ltd. 
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Eclipse Public License v1.0 
+ * which accompanies this distribution, and is available at 
+ * http://www.eclipse.org/legal/epl-v10.html  
+ *******************************************************************************/
+
+package org.zend.sdkcli.internal.commands;
+
+import org.zend.sdkcli.ParseError;
+import org.zend.sdklib.ZendProject;
+
+/**
+ * Concrete implementation of {@link AbstractCommand}. It represents
+ * create-project command. In the result of calling it new PHP project is
+ * created in defined location.
+ * 
+ * Command Parameters:
+ * <table border="1">
+ * <tr>
+ * <th>Parameter</th>
+ * <th>Required</th>
+ * <th>Argument</th>
+ * <th>Description</th>
+ * </tr>
+ * <tr>
+ * <td>name</td>
+ * <td>true</td>
+ * <td>String</td>
+ * <td>Project name.</td>
+ * </tr>
+ * <tr>
+ * <td>target</td>
+ * <td>false</td>
+ * <td>String</td>
+ * <td>Target ID.</td>
+ * </tr>
+ * <tr>
+ * <td>index</td>
+ * <td>false</td>
+ * <td>String</td>
+ * <td>Index name.</td>
+ * </tr>
+ * <tr>
+ * <td>path</td>
+ * <td>false</td>
+ * <td>String</td>
+ * <td>Path to the location where project should be created. If it is not
+ * specified, project will be created in the current location.</td>
+ * </tr>
+ * </table>
+ * 
+ * @author Wojciech Galanciak, 2011
+ * 
+ */
+public class UpdateProjectCommand extends AbstractCommand {
+
+	public static final String NAME = "name";
+	public static final String NO_SCRIPTS = "no_scripts";
+	public static final String DESTINATION = "destination";
+
+	public UpdateProjectCommand(CommandLine commandLine) throws ParseError {
+		super(commandLine);
+	}
+
+	@Override
+	public void setupOptions() {
+		addArgumentOption(NAME, false, "project name");
+		addArgumentOption(DESTINATION, false, "project destination");
+		addBooleanOption(NO_SCRIPTS, false, "create sample deployment scripts");
+	}
+
+	@Override
+	public boolean execute() {
+		String path = getValue(DESTINATION);
+		if (path == null) {
+			path = getValue(CommandOptions.CURR_DIR);
+		}
+		ZendProject project = new ZendProject(getValue(NAME), !Boolean.parseBoolean(getValue(NO_SCRIPTS)), path);
+		
+		try {
+			return project.update();
+		} catch (IllegalArgumentException e) {
+			getLogger().error(e.getMessage());
+			return false;
+		}
+	}
+}
