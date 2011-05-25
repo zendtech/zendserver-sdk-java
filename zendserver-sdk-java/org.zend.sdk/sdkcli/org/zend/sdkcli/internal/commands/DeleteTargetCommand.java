e9840d7378e608b0e32535a9c6106b9db3cac4fb
wgalanciak <wojciech.galanciak@gmail.com>
2011-05-25 08:33:05 +0000

    Minor comments changes

--- zendserver-sdk-java/org.zend.sdk/sdkcli/org/zend/sdkcli/internal/commands/DeleteTargetCommand.java
+++ zendserver-sdk-java/org.zend.sdk/sdkcli/org/zend/sdkcli/internal/commands/DeleteTargetCommand.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) May 18, 2011 Zend Technologies Ltd. 
+ * Copyright (c) May 24, 2011 Zend Technologies Ltd. 
  * All rights reserved. This program and the accompanying materials 
  * are made available under the terms of the Eclipse Public License v1.0 
  * which accompanies this distribution, and is available at 
@@ -12,7 +12,7 @@ import org.zend.sdklib.manager.TargetsManager;
 import org.zend.sdklib.target.IZendTarget;
 
 /**
- * Creating a new target
+ * Delete an existing target
  * 
  * @author Roy, 2011
  * 
@@ -20,7 +20,7 @@ import org.zend.sdklib.target.IZendTarget;
 public class DeleteTargetCommand extends TargetAwareCommand {
 
 	private static final String ID = "t";
-	
+
 	public DeleteTargetCommand(CommandLine commandLine) throws ParseError {
 		super(commandLine);
 	}
@@ -28,20 +28,20 @@ public class DeleteTargetCommand extends TargetAwareCommand {
 	@Override
 	public boolean execute() {
 		final String targetId = getValue(ID);
-		
+
 		TargetsManager tm = getTargetManager();
 		IZendTarget target = tm.getTargetById(targetId);
 		if (target == null) {
-			getLogger().error("Target '"+targetId+"' doesn't exist.");
+			getLogger().error("Target '" + targetId + "' doesn't exist.");
 			return false;
 		}
-		
+
 		IZendTarget removed = tm.remove(target);
 		if (removed == null) {
-			getLogger().error("Failed to remove target '"+targetId+"'");
+			getLogger().error("Failed to remove target '" + targetId + "'");
 			return false;
 		}
-		
+
 		return true;
 	}
 
