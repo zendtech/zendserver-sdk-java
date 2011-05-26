39834f3318a2a956c9358f725b807242895511b9
jacek.pospychala@gmail.com <jacek.pospychala@gmail.com@b75f36dc-5d77-e969-e1c6-c1f8c8b91a4e>
2011-05-26 14:20:43 +0000

    24: [cli] Add redeploy package command 26: [cli] Add update package command 28: [cli] Add remove package command

---
+++ org.zend.sdk/test/org/zend/sdk/test/sdkcli/commands/TestRemoveApplicationCommand.java
@@ -0,0 +1,61 @@
+package org.zend.sdk.test.sdkcli.commands;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
+
+import java.io.IOException;
+
+import org.junit.Test;
+import org.zend.sdk.test.AbstractWebApiTest;
+import org.zend.sdkcli.CommandFactory;
+import org.zend.sdkcli.ParseError;
+import org.zend.sdkcli.internal.commands.CommandLine;
+import org.zend.sdkcli.internal.commands.RemoveApplicationCommand;
+import org.zend.webapi.core.WebApiException;
+import org.zend.webapi.core.connection.data.ApplicationInfo;
+import org.zend.webapi.core.connection.data.IResponseData;
+import org.zend.webapi.internal.core.connection.auth.signature.SignatureException;
+
+public class TestRemoveApplicationCommand extends AbstractWebApiTest {
+
+	private String[] validCommand = new String[] { "remove", "application",
+			"-t", "0", "-id", "1" };
+
+	@Test
+	public void testExecute() throws WebApiException, IOException, ParseError {
+		CommandLine cmdLine = new CommandLine(validCommand);
+		RemoveApplicationCommand command = getCommand(cmdLine);
+		assertNotNull(command);
+		doReturn(application).when(command).getApplication();
+		when(
+				client.applicationRemove(anyInt())).thenReturn(
+				(ApplicationInfo) getResponseData("applicationRemove",
+						IResponseData.ResponseType.APPLICATION_INFO));
+		assertTrue(command.execute(cmdLine));
+	}
+
+	@Test
+	public void testExecuteTargetDisconnected() throws ParseError,
+			WebApiException, IOException {
+		CommandLine cmdLine = new CommandLine(validCommand);
+		RemoveApplicationCommand command = getCommand(cmdLine);
+		assertNotNull(command);
+		doReturn(application).when(command).getApplication();
+		when(client.applicationGetStatus()).thenThrow(
+				new SignatureException("testError"));
+		assertFalse(command.execute(cmdLine));
+	}
+
+	private RemoveApplicationCommand getCommand(CommandLine cmdLine)
+			throws ParseError {
+		RemoveApplicationCommand command = spy((RemoveApplicationCommand) CommandFactory
+				.createCommand(cmdLine));
+		return command;
+	}
+
+}
