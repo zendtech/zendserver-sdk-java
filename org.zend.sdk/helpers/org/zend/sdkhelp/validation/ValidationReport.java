ec407eb1d3c78691c671924eac43f81c1de0f053
ganoro@gmail.com <ganoro@gmail.com@b75f36dc-5d77-e969-e1c6-c1f8c8b91a4e>
2011-05-26 08:14:29 +0000

    update conflict validation

---
+++ org.zend.sdk/helpers/org/zend/sdkhelp/validation/ValidationReport.java
@@ -0,0 +1,78 @@
+/*******************************************************************************
+ * Copyright (c) May 18, 2011 Zend Technologies Ltd. 
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Eclipse Public License v1.0 
+ * which accompanies this distribution, and is available at 
+ * http://www.eclipse.org/legal/epl-v10.html  
+ *******************************************************************************/
+package org.zend.sdkhelp.validation;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.zend.sdkcli.CommandFactory;
+import org.zend.sdkcli.CommandType;
+import org.zend.sdkcli.ICommand;
+import org.zend.sdkcli.internal.logger.CliLogger;
+import org.zend.sdkcli.internal.options.DetectOptionUtility;
+import org.zend.sdklib.logger.Log;
+
+/**
+ * A tool to generate the most updated command line tables
+ * 
+ * @author Roy, 2011
+ * 
+ */
+public class ValidationReport {
+
+	public static void main(String[] args) throws IOException {
+
+		Log.getInstance().registerLogger(new CliLogger());
+		
+		PrintStream printStream = System.out;
+		if (args.length > 0) {
+			printStream = new PrintStream(new File(args[0]));
+		}
+		
+		printStream.println("<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>");
+		
+		// conflicts
+		checkConflicts(printStream);		
+
+		printStream.close();
+	}
+
+	private static void checkConflicts(PrintStream printStream) {
+		printStream.println("<conflicts>");
+		final CommandType[] values = CommandType.values();
+		for (CommandType commandType : values) {
+			final ICommand command = CommandFactory.createCommand(commandType);
+			final Class<? extends ICommand> class1 = command.getClass();
+
+			final Options options = new Options();
+			final List<org.zend.sdkcli.internal.options.Option> all = DetectOptionUtility.getOptions(class1, false);
+
+			
+			Set<String> opts = new HashSet<String>();
+			for (org.zend.sdkcli.internal.options.Option object : all) {
+				if (opts.contains(object.opt())) {
+					printStream.print("<error message=\"Option conflict in ");
+					printStream.print(command.getClass().getName());
+					printStream.println("\">");
+					printStream.print("\tconflict with option: -");
+					printStream.println(object.opt());
+					printStream.print("</error>");
+				} 
+				opts.add(object.opt());
+			}
+		}
+		printStream.println("</conflicts>");
+	}
+
+}
--- org.zend.sdk/sdkcli/org/zend/sdkcli/internal/options/DetectOptionUtility.java
+++ org.zend.sdk/sdkcli/org/zend/sdkcli/internal/options/DetectOptionUtility.java
@@ -8,8 +8,9 @@
 package org.zend.sdkcli.internal.options;
 
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
 
-import org.apache.commons.cli.OptionBuilder;
 import org.apache.commons.cli.Options;
 
 /**
@@ -30,39 +31,74 @@ public class DetectOptionUtility {
 			boolean specific) {
 		final Method[] methods = specific ? subject.getDeclaredMethods()
 				: subject.getMethods();
+
 		for (Method method : methods) {
-			if (method.getName().startsWith(STRING_OPTION)
-					&& method.getAnnotation(Option.class) != null) {
-				final Option a = method.getAnnotation(Option.class);
+			if (isString(method) && hasOptionAnnotation(method)) {
+				addString(options, method);
+			} else if (isBoolean(method) && hasOptionAnnotation(method)) {
+				addBoolean(options, method);
 
-				// create option object
-				final org.apache.commons.cli.Option o = new org.apache.commons.cli.Option(
-						a.opt(), a.description());
-				if (a.longOpt() != null && a.longOpt().length() > 0) {
-					o.setLongOpt(a.longOpt());
-				}
-				o.setArgs(a.numberOfArgs());
-				o.setRequired(a.required());
-				o.setType(a.type());
-
-				// assign to options list
-				options.addOption(o);
-			} else if (method.getName().startsWith(BOOLEAN_OPTION)
-					&& method.getAnnotation(Option.class) != null) {
-				final Option a = method.getAnnotation(Option.class);
-
-				// create option object
-				final org.apache.commons.cli.Option o = new org.apache.commons.cli.Option(
-						a.opt(), a.description());
-				if (a.longOpt() != null && a.longOpt().length() > 0) {
-					o.setLongOpt(a.longOpt());
-				}
-				o.setArgs(0);
-				o.setRequired(a.required());
-
-				// assign to options list options.addOption(o);
-				options.addOption(o);
 			}
 		}
+	}
+
+	public static List<Option> getOptions(Class subject, boolean specific) {
+
+		final Method[] methods = specific ? subject.getDeclaredMethods()
+				: subject.getMethods();
+
+		List<Option> result = new ArrayList<Option>();
+		for (Method method : methods) {
+			if (hasOptionAnnotation(method)
+					&& (isString(method) || isBoolean(method))) {
+				result.add(method.getAnnotation(Option.class));
+			}
+		}
+		return result;
+	}
+
+	private final static boolean hasOptionAnnotation(Method method) {
+		return method.getAnnotation(Option.class) != null;
+	}
+
+	private static void addBoolean(Options options, Method method) {
+		final Option a = method.getAnnotation(Option.class);
+
+		// create option object
+		final org.apache.commons.cli.Option o = new org.apache.commons.cli.Option(
+				a.opt(), a.description());
+		if (a.longOpt() != null && a.longOpt().length() > 0) {
+			o.setLongOpt(a.longOpt());
+		}
+		o.setArgs(0);
+		o.setRequired(a.required());
+
+		// assign to options list options.addOption(o);
+		options.addOption(o);
+	}
+
+	private static void addString(Options options, Method method) {
+		final Option a = method.getAnnotation(Option.class);
+
+		// create option object
+		final org.apache.commons.cli.Option o = new org.apache.commons.cli.Option(
+				a.opt(), a.description());
+		if (a.longOpt() != null && a.longOpt().length() > 0) {
+			o.setLongOpt(a.longOpt());
+		}
+		o.setArgs(a.numberOfArgs());
+		o.setRequired(a.required());
+		o.setType(a.type());
+
+		// assign to options list
+		options.addOption(o);
+	}
+
+	private static boolean isBoolean(Method method) {
+		return method.getName().startsWith(BOOLEAN_OPTION);
+	}
+
+	private static boolean isString(Method method) {
+		return method.getName().startsWith(STRING_OPTION);
 	}
 }
