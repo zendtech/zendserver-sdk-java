92a6e6c993f89abffb973207d2a968ea55ab3e2e
jacek.pospychala@gmail.com <jacek.pospychala@gmail.com@b75f36dc-5d77-e969-e1c6-c1f8c8b91a4e>
2011-05-26 09:45:29 +0000

    project create/update tests should use temp dir

--- org.zend.sdk/test/org/zend/sdk/test/sdklib/library/TestZendProject.java
+++ org.zend.sdk/test/org/zend/sdk/test/sdklib/library/TestZendProject.java
@@ -4,86 +4,89 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
+import java.io.IOException;
 
 import org.junit.Test;
 import org.zend.sdk.test.AbstractTest;
+import org.zend.sdk.test.sdkcli.commands.TestCreateProjectCommand;
 import org.zend.sdkcli.ParseError;
 import org.zend.sdklib.ZendProject;
 
 public class TestZendProject extends AbstractTest {
 
 	@Test
-	public void testZendProjectCreation1() throws ParseError {
-		new File("testdir1").mkdir();
-		ZendProject project = new ZendProject("name", true, "testdir1");
+	public void testZendProjectCreation1() throws ParseError, IOException {
+		String dirName = TestCreateProjectCommand.getTempFileName();
+		ZendProject project = new ZendProject("name", true, dirName);
 		assertTrue(project.create());
 		
-		assertTrue(new File("testdir1/descriptor.xml").exists());
-		assertTrue(new File("testdir1/public/index.html").exists());
-		assertTrue(new File("testdir1/scripts/post_activate.php").exists());
-		assertTrue(new File("testdir1/scripts/post_deactivate.php").exists());
-		assertTrue(new File("testdir1/scripts/post_stage.php").exists());
-		assertTrue(new File("testdir1/scripts/post_unstage.php").exists());
-		assertTrue(new File("testdir1/scripts/pre_activate.php").exists());
-		assertTrue(new File("testdir1/scripts/pre_deactivate.php").exists());
-		assertTrue(new File("testdir1/scripts/pre_stage.php").exists());
-		assertTrue(new File("testdir1/scripts/pre_unstage.php").exists());
+		assertTrue(new File(dirName+"/descriptor.xml").exists());
+		assertTrue(new File(dirName+"/public/index.html").exists());
+		assertTrue(new File(dirName+"/scripts/post_activate.php").exists());
+		assertTrue(new File(dirName+"/scripts/post_deactivate.php").exists());
+		assertTrue(new File(dirName+"/scripts/post_stage.php").exists());
+		assertTrue(new File(dirName+"/scripts/post_unstage.php").exists());
+		assertTrue(new File(dirName+"/scripts/pre_activate.php").exists());
+		assertTrue(new File(dirName+"/scripts/pre_deactivate.php").exists());
+		assertTrue(new File(dirName+"/scripts/pre_stage.php").exists());
+		assertTrue(new File(dirName+"/scripts/pre_unstage.php").exists());
 	}
 
 	@Test
-	public void testZendProjectCreation2() throws ParseError {
-		ZendProject project = new ZendProject("name", true, null);
+	public void testZendProjectCreation2() throws ParseError, IOException {
+		String dirName = TestCreateProjectCommand.getTempFileName();
+		ZendProject project = new ZendProject("name", true, dirName);
 		assertTrue(project.create());
 		
-		assertTrue(new File("descriptor.xml").exists());
-		assertTrue(new File("public/index.html").exists());
-		assertTrue(new File("scripts/post_activate.php").exists());
-		assertTrue(new File("scripts/post_deactivate.php").exists());
-		assertTrue(new File("scripts/post_stage.php").exists());
-		assertTrue(new File("scripts/post_unstage.php").exists());
-		assertTrue(new File("scripts/pre_activate.php").exists());
-		assertTrue(new File("scripts/pre_deactivate.php").exists());
-		assertTrue(new File("scripts/pre_stage.php").exists());
-		assertTrue(new File("scripts/pre_unstage.php").exists());
+		assertTrue(new File(dirName+"/descriptor.xml").exists());
+		assertTrue(new File(dirName+"/public/index.html").exists());
+		assertTrue(new File(dirName+"/scripts/post_activate.php").exists());
+		assertTrue(new File(dirName+"/scripts/post_deactivate.php").exists());
+		assertTrue(new File(dirName+"/scripts/post_stage.php").exists());
+		assertTrue(new File(dirName+"/scripts/post_unstage.php").exists());
+		assertTrue(new File(dirName+"/scripts/pre_activate.php").exists());
+		assertTrue(new File(dirName+"/scripts/pre_deactivate.php").exists());
+		assertTrue(new File(dirName+"/scripts/pre_stage.php").exists());
+		assertTrue(new File(dirName+"/scripts/pre_unstage.php").exists());
 	}
 
 	@Test
-	public void testZendProjectCreation3() throws ParseError {
-		new File("testdir3").mkdir();
-		ZendProject project = new ZendProject("name", false, "testdir3");
+	public void testZendProjectCreation3() throws ParseError, IOException {
+		String dirName = TestCreateProjectCommand.getTempFileName();
+		ZendProject project = new ZendProject("name", false, dirName);
 		assertTrue(project.create());
 		
-		assertTrue(new File("testdir3/descriptor.xml").exists());
-		assertTrue(new File("testdir3/public/index.html").exists());
-		assertFalse(new File("testdir3/scripts/post_activate.php").exists());
-		assertFalse(new File("testdir3/scripts/post_deactivate.php").exists());
-		assertFalse(new File("testdir3/scripts/post_stage.php").exists());
-		assertFalse(new File("testdir3/scripts/post_unstage.php").exists());
-		assertFalse(new File("testdir3/scripts/pre_activate.php").exists());
-		assertFalse(new File("testdir3/scripts/pre_deactivate.php").exists());
-		assertFalse(new File("testdir3/scripts/pre_stage.php").exists());
-		assertFalse(new File("testdir3/scripts/pre_unstage.php").exists());
+		assertTrue(new File(dirName+"/descriptor.xml").exists());
+		assertTrue(new File(dirName+"/public/index.html").exists());
+		assertFalse(new File(dirName+"/scripts/post_activate.php").exists());
+		assertFalse(new File(dirName+"/scripts/post_deactivate.php").exists());
+		assertFalse(new File(dirName+"/scripts/post_stage.php").exists());
+		assertFalse(new File(dirName+"/scripts/post_unstage.php").exists());
+		assertFalse(new File(dirName+"/scripts/pre_activate.php").exists());
+		assertFalse(new File(dirName+"/scripts/pre_deactivate.php").exists());
+		assertFalse(new File(dirName+"/scripts/pre_stage.php").exists());
+		assertFalse(new File(dirName+"/scripts/pre_unstage.php").exists());
 	}
 
 
 	@Test
-	public void testZendProjectUpdate1() throws ParseError {
-		new File("testdir4").mkdir();
-		ZendProject project = new ZendProject("name", false, "testdir4");
+	public void testZendProjectUpdate1() throws ParseError, IOException {
+		String dirName = TestCreateProjectCommand.getTempFileName();
+		ZendProject project = new ZendProject("name", false, dirName);
 		assertTrue(project.update());
 		
-		assertTrue(new File("testdir4/descriptor.xml").exists());
-		assertFalse(new File("testdir4/public").exists());
-		assertFalse(new File("testdir4/public/index.html").exists());
-		assertFalse(new File("testdir4/scripts").exists());
-		assertFalse(new File("testdir4/scripts/post_activate.php").exists());
-		assertFalse(new File("testdir4/scripts/post_deactivate.php").exists());
-		assertFalse(new File("testdir4/scripts/post_stage.php").exists());
-		assertFalse(new File("testdir4/scripts/post_unstage.php").exists());
-		assertFalse(new File("testdir4/scripts/pre_activate.php").exists());
-		assertFalse(new File("testdir4/scripts/pre_deactivate.php").exists());
-		assertFalse(new File("testdir4/scripts/pre_stage.php").exists());
-		assertFalse(new File("testdir4/scripts/pre_unstage.php").exists());
+		assertTrue(new File(dirName+"/descriptor.xml").exists());
+		assertFalse(new File(dirName+"/public").exists());
+		assertFalse(new File(dirName+"/public/index.html").exists());
+		assertFalse(new File(dirName+"/scripts").exists());
+		assertFalse(new File(dirName+"/scripts/post_activate.php").exists());
+		assertFalse(new File(dirName+"/scripts/post_deactivate.php").exists());
+		assertFalse(new File(dirName+"/scripts/post_stage.php").exists());
+		assertFalse(new File(dirName+"/scripts/post_unstage.php").exists());
+		assertFalse(new File(dirName+"/scripts/pre_activate.php").exists());
+		assertFalse(new File(dirName+"/scripts/pre_deactivate.php").exists());
+		assertFalse(new File(dirName+"/scripts/pre_stage.php").exists());
+		assertFalse(new File(dirName+"/scripts/pre_unstage.php").exists());
 	}
 	
 }
