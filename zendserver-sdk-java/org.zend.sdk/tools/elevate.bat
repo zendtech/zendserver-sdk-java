ec07d52a3dc5e365daeb7fa46cfd9e797216e5bf
ganoro@gmail.com <ganoro@gmail.com@b75f36dc-5d77-e969-e1c6-c1f8c8b91a4e>
2011-05-27 10:52:50 +0000

    fix elevate script

--- zendserver-sdk-java/org.zend.sdk/tools/elevate.bat
+++ zendserver-sdk-java/org.zend.sdk/tools/elevate.bat
@@ -1,9 +1,9 @@
 @echo off
 if "%PROCESSOR_ARCHITECTURE%" == "x86" (
-	@start ..\lib\x86\elevate.exe "CD %CD% & %*
+	@start ..\lib\x86\elevate.exe -k -wait "CD %CD% & %* & exit
 ) else (
     if "%PROCESSOR_ARCHITECTURE%" == "AMD64" (
-		@start ..\lib\x86_64\elevate.exe "CD %CD% & %*
+		@start ..\lib\x86_64\elevate.exe -k -wait "CD %CD% & %* & exit
     ) else (
 		echo Unsupported platform.
 		@pause
