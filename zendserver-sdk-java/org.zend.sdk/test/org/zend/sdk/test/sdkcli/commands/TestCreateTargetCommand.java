387049d627573fa7c788715093362f14666b8a09
wgalanciak <wojciech.galanciak@gmail.com>
2011-05-30 09:56:07 +0000

    Improve logging and tests for delete target command

--- zendserver-sdk-java/org.zend.sdk/test/org/zend/sdk/test/sdkcli/commands/TestCreateTargetCommand.java
+++ zendserver-sdk-java/org.zend.sdk/test/org/zend/sdk/test/sdkcli/commands/TestCreateTargetCommand.java
@@ -3,7 +3,7 @@ package org.zend.sdk.test.sdkcli.commands;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
-import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.spy;
 
@@ -14,21 +14,20 @@ import org.zend.sdkcli.CommandFactory;
 import org.zend.sdkcli.ParseError;
 import org.zend.sdkcli.internal.commands.CommandLine;
 import org.zend.sdkcli.internal.commands.CreateTargetCommand;
+import org.zend.sdklib.target.IZendTarget;
 import org.zend.webapi.core.WebApiException;
 
 public class TestCreateTargetCommand extends AbstractTargetCommandTest {
 
 	private String[] validCommand = new String[] { "create", "target", "-t",
-			"1", "-k", "mykey", "-s", "123456", "-h",
-			"http://test1test" };
+			"1", "-k", "mykey", "-s", "123456", "-h", "http://test1test" };
 
 	@Test
 	public void testExecute() throws ParseError, WebApiException {
 		CommandLine cmdLine = new CommandLine(validCommand);
 		CreateTargetCommand command = getCommand(cmdLine);
 		assertNotNull(command);
-		doReturn(getTarget()).when(manager).createTarget(anyString(),
-				anyString(), anyString(), anyString());
+		doReturn(getTarget()).when(manager).add(any(IZendTarget.class));
 		assertTrue(command.execute(cmdLine));
 	}
 
@@ -37,8 +36,7 @@ public class TestCreateTargetCommand extends AbstractTargetCommandTest {
 		CommandLine cmdLine = new CommandLine(validCommand);
 		CreateTargetCommand command = getCommand(cmdLine);
 		assertNotNull(command);
-		doReturn(null).when(manager).createTarget(anyString(), anyString(),
-				anyString(), anyString());
+		doReturn(null).when(manager).add(any(IZendTarget.class));
 		assertFalse(command.execute(cmdLine));
 	}
 
@@ -46,8 +44,8 @@ public class TestCreateTargetCommand extends AbstractTargetCommandTest {
 	public void testExecuteInvalidUrl() throws ParseError, WebApiException,
 			MalformedURLException {
 		CommandLine cmdLine = new CommandLine(new String[] { "create",
-			"target", "-t", "1", "-key", "mykey", "-secretKey", "123456",
-			"-host", "a111:/\test1test" });
+				"target", "-t", "1", "-key", "mykey", "-secretKey", "123456",
+				"-host", "a111:/\test1test" });
 		CreateTargetCommand command = getCommand(cmdLine);
 		assertNotNull(command);
 		assertFalse(command.execute(cmdLine));
