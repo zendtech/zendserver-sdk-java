72980e397e46009e666b6d787df7e1058cd23c13
wgalanciak <wojciech.galanciak@gmail.com>
2011-05-30 13:03:31 +0000

    Project is not created in a current location

--- org.zend.sdk/tools/zend.bat
+++ org.zend.sdk/tools/zend.bat
@@ -8,19 +8,17 @@ rem http://www.eclipse.org/legal/epl-v10.html
 rem Don't modify the caller's environment
 setlocal
 
-cd /d %~dp0
-
 rem Check we have a valid Java.exe in the path.
 set java_exe=
-call ../tools/find_java.bat
+call %~dp0/../tools/find_java.bat
 if not defined java_exe goto :EOF
 
 rem Set registry Jar path based on current architecture (x86 or x86_64)
-for /f %%a in ('%java_exe% -jar ..\lib\archquery.jar') do set registry_path=..\lib\%%a
+for /f %%a in ('%java_exe% -jar %~dp0\..\lib\archquery.jar') do set registry_path=%~dp0\..\lib\%%a
 
-set jar_path=../lib/*
+set jar_path=%~dp0/../lib/*
 
 rem Finally exec the java program and end here.
-call %java_exe% -Djava.library.path="%registry_path%" -classpath "%jar_path%;../bin" org.zend.sdkcli.Main %*
+call %java_exe% -Djava.library.path="%registry_path%" -classpath "%jar_path%;%~dp0/../bin" org.zend.sdkcli.Main %*
 
 :EOF
