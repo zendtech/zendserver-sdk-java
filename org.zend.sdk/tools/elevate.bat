1395bd5d5ba21e357bf71ddefdfedac2360ceec5
ganoro@gmail.com <ganoro@gmail.com@b75f36dc-5d77-e969-e1c6-c1f8c8b91a4e>
2011-05-30 08:56:01 +0000

    remove the "del test" which can't be executed by simple user

--- org.zend.sdk/tools/elevate.bat
+++ org.zend.sdk/tools/elevate.bat
@@ -1,9 +1,9 @@
 @echo off
 if "%PROCESSOR_ARCHITECTURE%" == "x86" (
-  %~d0%~p0..\lib\x86\elevate.exe -wait -k "CD %CD% & %* > out & exit" & type out % del out
+  %~d0%~p0..\lib\x86\elevate.exe -wait -k "CD %CD% & %* > out & exit" & type out 
 ) else (
   if "%PROCESSOR_ARCHITECTURE%" == "AMD64" (
-    %~d0%~p0..\lib\x86_64\elevate.exe -wait -k "CD %CD% & %* > out & exit" & type out % del out
+    %~d0%~p0..\lib\x86_64\elevate.exe -wait -k "CD %CD% & %* > out & exit" & type out 
   ) else (
     echo Unsupported platform.
     @pause
