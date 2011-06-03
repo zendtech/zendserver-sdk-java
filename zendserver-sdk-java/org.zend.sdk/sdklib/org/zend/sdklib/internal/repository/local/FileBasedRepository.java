a95aa22e30164918adc16dcce1d42e65e4e07a66
ganoro@gmail.com <ganoro@gmail.com@b75f36dc-5d77-e969-e1c6-c1f8c8b91a4e>
2011-06-03 07:39:24 +0000

    1. add basic support for local repository 2. add test for basic repository

---
+++ zendserver-sdk-java/org.zend.sdk/sdklib/org/zend/sdklib/internal/repository/local/FileBasedRepository.java
@@ -0,0 +1,65 @@
+/*******************************************************************************
+ * Copyright (c) May 18, 2011 Zend Technologies Ltd. 
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Eclipse Public License v1.0 
+ * which accompanies this distribution, and is available at 
+ * http://www.eclipse.org/legal/epl-v10.html  
+ *******************************************************************************/
+package org.zend.sdklib.internal.repository.local;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.zend.sdklib.internal.repository.AbstractStreamRepository;
+
+/**
+ * an implementation for local Application Repository, as described in
+ * http://code.google.com/p/zend-sdk/wiki/RepositorySpec
+ * 
+ * Structure of this repository:
+ * 
+ * <pre>
+ *  	<root>
+ *  	   site.xml - repository descriptor
+ *  	   /applications
+ *              /<application_id>_<application_version>
+ *                 <application_id>_<application_version>.zpk
+ *              /<application_id>_<application_version>
+ *                 <application_id>_<application_version>.zpk
+ *              /<application_id>_<application_version>
+ *                 <application_id>_<application_version>.zpk
+ *  	   /resources
+ *              myicon.png
+ *              mytext.txt
+ * 
+ * </pre>
+ * 
+ * @author Roy, 2011
+ */
+public class FileBasedRepository extends AbstractStreamRepository {
+
+	private static final String SITE_XML = "site.xml";
+	/**
+	 * Base dir for the repository
+	 */
+	private final File basedir;
+
+	public FileBasedRepository(File basedir) {
+		super();
+		this.basedir = basedir;
+	}
+
+	@Override
+	public InputStream getSiteStream() throws IOException {
+		return getArtifactStream(SITE_XML);
+	}
+
+	@Override
+	public InputStream getArtifactStream(String path) throws IOException {
+		final File file = new File(basedir, path);
+		return new FileInputStream(file);
+	}
+
+}
