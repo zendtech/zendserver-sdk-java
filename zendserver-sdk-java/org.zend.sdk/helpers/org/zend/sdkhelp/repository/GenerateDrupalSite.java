8b0d0e80576970e0af53554415f14622b1f438b5
ganoro@gmail.com <ganoro@gmail.com@b75f36dc-5d77-e969-e1c6-c1f8c8b91a4e>
2011-06-03 10:15:13 +0000

    [Helpers] generate a sample site

--- zendserver-sdk-java/org.zend.sdk/helpers/org/zend/sdkhelp/repository/GenerateDrupalSite.java
+++ zendserver-sdk-java/org.zend.sdk/helpers/org/zend/sdkhelp/repository/GenerateDrupalSite.java
@@ -5,21 +5,51 @@
  * which accompanies this distribution, and is available at 
  * http://www.eclipse.org/legal/epl-v10.html  
  *******************************************************************************/
-package org.zend.sdkhelp.examples;
+package org.zend.sdkhelp.repository;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
+import javax.xml.bind.PropertyException;
 
 import org.zend.sdklib.repository.site.Application;
 import org.zend.sdklib.repository.site.CategoryDef;
 import org.zend.sdklib.repository.site.ObjectFactory;
+import org.zend.sdklib.repository.site.ProviderDef;
 import org.zend.sdklib.repository.site.Site;
 
+/**
+ * Generates a sample repository site descriptor
+ * 
+ * @author Roy, 2011
+ */
 public class GenerateDrupalSite {
 
-	
-	public static void main(String[] args) throws JAXBException {
+	public static void main(String[] args) throws JAXBException,
+			FileNotFoundException {
+		PrintStream printStream = System.out;
+		if (args.length > 0) {
+			printStream = new PrintStream(new File(args[0]));
+		}
+
+		final Site s = createDrupalSite();
+		printSite(printStream, s);
+	}
+
+	private static void printSite(PrintStream printStream, final Site s)
+			throws JAXBException, PropertyException {
+		JAXBContext jc = JAXBContext
+				.newInstance("org.zend.sdklib.repository.site");
+		Marshaller m = jc.createMarshaller();
+		m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+		m.marshal(s, printStream);
+	}
+
+	private static Site createDrupalSite() {
 		ObjectFactory factory = new ObjectFactory();
 		final Site s = factory.createSite();
 		s.setDescription("This is my first drupal site");
@@ -28,18 +58,18 @@ public class GenerateDrupalSite {
 		a.setId("my.drupal");
 		a.setName("drupal");
 		a.setCategory("cms");
+		a.setProvider("me");
 		s.getApplication().add(a);
-		
+
 		final CategoryDef c = factory.createCategoryDef();
 		c.setName("cms");
 		c.setLabel("Content Managment System");
 		s.getCategoryDef().add(c);
-		
-		JAXBContext jc = JAXBContext.newInstance( "org.zend.sdklib.repository.site" );
-		Marshaller m = jc.createMarshaller();
-		m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, 
-		  Boolean.TRUE);
-		m.marshal( s, System.out );
-	}
 
+		final ProviderDef p = factory.createProviderDef();
+		p.setName("me");
+		p.setLabel("I am \"Me\"");
+		s.getProviderDef().add(p);
+		return s;
+	}
 }
