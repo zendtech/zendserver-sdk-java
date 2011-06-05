15b20b53e4261b15a80891cac82c777a7cb09f1d
ganoro@gmail.com <ganoro@gmail.com@b75f36dc-5d77-e969-e1c6-c1f8c8b91a4e>
2011-06-05 12:10:46 +0000

    add repository factory

---
+++ org.zend.sdk/sdklib/org/zend/sdklib/repository/RepositoryFactory.java
@@ -0,0 +1,77 @@
+/*******************************************************************************
+ * Copyright (c) May 18, 2011 Zend Technologies Ltd. 
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Eclipse Public License v1.0 
+ * which accompanies this distribution, and is available at 
+ * http://www.eclipse.org/legal/epl-v10.html  
+ *******************************************************************************/
+package org.zend.sdklib.repository;
+
+import java.io.File;
+
+import org.zend.sdklib.internal.repository.http.HttpRepository;
+import org.zend.sdklib.internal.repository.local.FileBasedRepository;
+
+/**
+ * Creates a repository client for a given string
+ * 
+ * @author Roy, 2011
+ */
+public class RepositoryFactory {
+
+	private final static String HTTP = "http://";
+	private final static String HTTPS = "https://";
+	private final static String FILE = "file:/";
+
+	/**
+	 * *
+	 * 
+	 * <pre>
+	 * &quot;file:/&quot;
+	 * </pre>
+	 * 
+	 * - for local repository
+	 * 
+	 * <pre>
+	 * "http:/" or "https://"
+	 * </pre>
+	 * 
+	 * - for remote repository
+	 * 
+	 * @param url
+	 * @return
+	 */
+	final public static IRepository createRepository(String url) {
+
+		String path = path(url, false, HTTP, HTTPS);
+		if (null != path) {
+			return new HttpRepository(path);
+		}
+
+		path = path(url, true, FILE);
+		if (null != path) {
+			return new FileBasedRepository(new File(path));
+		}
+
+		return null;
+	}
+
+	/**
+	 * finds the path according to the given URL hints. Trims the hints or not
+	 * according to the trim parameter
+	 * 
+	 * @param url
+	 * @param trim
+	 * @param startsWith
+	 * @return
+	 */
+	private static String path(String url, boolean trim, String... startsWith) {
+		for (String s : startsWith) {
+			if (url.length() > s.length()
+					&& s.equalsIgnoreCase(url.substring(0, s.length()))) {
+				return trim ? url.substring(s.length()) : url;
+			}
+		}
+		return null;
+	}
+}
