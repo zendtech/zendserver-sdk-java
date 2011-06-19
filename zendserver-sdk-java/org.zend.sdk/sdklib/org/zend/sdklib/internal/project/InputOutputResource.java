c8c11245c1e59fd643783eda14ded0db2a6f7cb3
ganoro@gmail.com <ganoro@gmail.com@b75f36dc-5d77-e969-e1c6-c1f8c8b91a4e>
2011-06-19 07:34:05 +0000

    add scripts handling

---
+++ zendserver-sdk-java/org.zend.sdk/sdklib/org/zend/sdklib/internal/project/InputOutputResource.java
@@ -0,0 +1,58 @@
+/*******************************************************************************
+ * Copyright (c) May 18, 2011 Zend Technologies Ltd. 
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Eclipse Public License v1.0 
+ * which accompanies this distribution, and is available at 
+ * http://www.eclipse.org/legal/epl-v10.html  
+ *******************************************************************************/
+package org.zend.sdklib.internal.project;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import org.zend.sdklib.internal.project.ScriptsWriter.DeploymentScriptTypes;
+
+/**
+ * Helps wiring stream from one place to another
+ */
+public class InputOutputResource {
+
+	private final InputStream is;
+	private final OutputStream os;
+
+	/**
+	 * @param is
+	 * @param os
+	 */
+	public InputOutputResource(InputStream is, OutputStream os) {
+		this.is = is;
+		this.os = os;
+	}
+
+	/**
+	 * @param scripttype 
+	 * @param os
+	 */
+	public InputOutputResource(DeploymentScriptTypes type, OutputStream os) {
+		this(InputOutputResource.class.getResourceAsStream(type.getFilename()),
+				os);
+	}
+
+	public void copy() throws IOException {
+		try {
+			byte[] buf = new byte[4098];
+			int c;
+			while ((c = is.read(buf)) > 0) {
+				os.write(buf, 0, c);
+			}
+		} finally {
+			if (is != null) {
+				is.close();
+			}
+			if (os != null) {
+				os.close();
+			}
+		}
+	}
+}
