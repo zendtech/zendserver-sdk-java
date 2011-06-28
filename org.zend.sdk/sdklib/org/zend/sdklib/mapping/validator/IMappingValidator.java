ca2dcd32ea8e7cfe8738c9b2c46e06d5ce01964e
wojciech.galanciak@gmail.com <wojciech.galanciak@gmail.com@b75f36dc-5d77-e969-e1c6-c1f8c8b91a4e>
2011-06-28 10:33:13 +0000

    Mapping validator

---
+++ org.zend.sdk/sdklib/org/zend/sdklib/mapping/validator/IMappingValidator.java
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ * Copyright (c) Jun 28, 2011 Zend Technologies Ltd. 
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Eclipse Public License v1.0 
+ * which accompanies this distribution, and is available at 
+ * http://www.eclipse.org/legal/epl-v10.html  
+ *******************************************************************************/
+package org.zend.sdklib.mapping.validator;
+
+import java.io.InputStream;
+
+/**
+ * Interface for parsing and validating resource mapping properites file.
+ * 
+ * @author Wojciech Galanciak, 2011
+ * 
+ */
+public interface IMappingValidator {
+
+	/**
+	 * Parses provided properties input stream. All errors are collected and if
+	 * their number is greater than 0 then {@link MappingParseException} is
+	 * thrown.
+	 * 
+	 * @param stream
+	 * @return
+	 * @throws MappingParseException
+	 */
+	boolean parse(InputStream stream) throws MappingParseException;
+
+}
