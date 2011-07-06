f79ccc2e72405c8280cc33dd578e5f277d5b176e
Jacek Pospychala <jacek.pospychala>
2011-07-06 21:32:49 +0000

    enable validation

--- zendserver-deployment-eclipse/org.zend.php.zendserver.deployment.core/src/org/zend/php/zendserver/deployment/core/IncrementalDeploymentBuilder.java
+++ zendserver-deployment-eclipse/org.zend.php.zendserver.deployment.core/src/org/zend/php/zendserver/deployment/core/IncrementalDeploymentBuilder.java
@@ -2,14 +2,22 @@ package org.zend.php.zendserver.deployment.core;
 
 import java.util.Map;
 
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IResourceDelta;
+import org.eclipse.core.resources.IResourceDeltaVisitor;
 import org.eclipse.core.resources.IncrementalProjectBuilder;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.zend.php.zendserver.deployment.core.descriptor.DescriptorContainerManager;
+import org.zend.php.zendserver.deployment.core.descriptor.IDescriptorContainer;
+import org.zend.php.zendserver.deployment.core.internal.validation.Validator;
 
 public class IncrementalDeploymentBuilder extends IncrementalProjectBuilder {
 
-	public static final String ID = DeploymentCore.PLUGIN_ID + ".DeploymentBuilder";
+	public static final String ID = DeploymentCore.PLUGIN_ID + ".DeploymentBuilder"; //$NON-NLS-1$
 
 	public IncrementalDeploymentBuilder() {
 		// TODO Auto-generated constructor stub
@@ -18,9 +26,39 @@ public class IncrementalDeploymentBuilder extends IncrementalProjectBuilder {
 	@Override
 	protected IProject[] build(int kind, Map<String, String> args,
 			IProgressMonitor monitor) throws CoreException {
-		//System.out.println("Build "+getProject()+" "+getDelta(getProject()).getResource());
+		IResourceDelta delta = getDelta(getProject());
+		delta.accept(new IResourceDeltaVisitor() {
+			
+			public boolean visit(IResourceDelta delta) throws CoreException {
+				IResource resource = delta.getResource();
+				if ((resource instanceof IFile) && (DescriptorContainerManager.DESCRIPTOR_PATH.equals(resource.getName()))) {
+					validateDescriptor((IFile)resource);
+				}
+				
+				if (resource instanceof IProject) {
+					return true;
+				}
+				
+				if (resource instanceof IFolder) {
+					return false;
+				}
+				return false;
+			}
+		});
 		
 		return null;
+	}
+
+	protected void validateDescriptor(IFile file) {
+		System.out.println("validating descriptor");
+		
+		IDescriptorContainer model = DescriptorContainerManager.getService()
+				.openDescriptorContainer(file);
+		
+		Validator validator = new Validator();
+		validator.validate(model.getDescriptorModel());
+		
+		// TODO Add markers
 	}
 
 }
