89dbbd588fa901d208f578210f0cae240a07c77a
Jacek Pospychala <jacek.pospychala>
2011-07-06 10:47:25 +0000

    externalize strings

---
+++ zendserver-deployment-eclipse/org.zend.php.zendserver.deployment.ui/src/org/zend/php/zendserver/deployment/ui/Messages.java
@@ -0,0 +1,116 @@
+package org.zend.php.zendserver.deployment.ui;
+
+import org.eclipse.osgi.util.NLS;
+
+public class Messages extends NLS {
+	private static final String BUNDLE_NAME = "org.zend.php.zendserver.deployment.ui.messages"; //$NON-NLS-1$
+	public static String AppTreeSection_AppDirMapping;
+	public static String AppTreeSection_InfoAboutAppdirFolder;
+	public static String DependenciesMasterDetailsProvider_Add;
+	public static String DependenciesMasterDetailsProvider_DependencyType;
+	public static String DependenciesMasterDetailsProvider_Description;
+	public static String DeploymentDescriptorEditor_Dependencies;
+	public static String DeploymentDescriptorEditor_Package;
+	public static String DeploymentDescriptorEditor_Parameters;
+	public static String DeploymentDescriptorEditor_Variables;
+	public static String DeploymentDescriptorLabelProvider_conflictsWith;
+	public static String DeploymentDescriptorLabelProvider_Equals;
+	public static String DeploymentDescriptorLabelProvider_From;
+	public static String DeploymentDescriptorLabelProvider_greaterThan;
+	public static String DeploymentDescriptorLabelProvider_lessThan;
+	public static String DeploymentDescriptorLabelProvider_PHP;
+	public static String DeploymentDescriptorLabelProvider_PHPDirective;
+	public static String DeploymentDescriptorLabelProvider_PHPExtension;
+	public static String DeploymentDescriptorLabelProvider_PHPVersion;
+	public static String DeploymentDescriptorLabelProvider_Required;
+	public static String DeploymentDescriptorLabelProvider_To;
+	public static String DeploymentDescriptorLabelProvider_ZendFramework;
+	public static String DeploymentDescriptorLabelProvider_ZendFrameworkVersion;
+	public static String DeploymentDescriptorLabelProvider_ZendServer;
+	public static String DeploymentDescriptorLabelProvider_ZendServerComponent;
+	public static String DeploymentDescriptorLabelProvider_ZendServerVersion;
+	public static String DescriptorMasterDetailsBlock_Add;
+	public static String DescriptorMasterDetailsBlock_Remove;
+	public static String DirectiveDependencyDetailsPage_Directive;
+	public static String DirectiveDependencyDetailsPage_DirectiveDependencyDetails;
+	public static String DirectiveDependencyDetailsPage_SpecifyDirectiveProperties;
+	public static String ExtensionDependencyDetailsPage_extensionDependencyDetails;
+	public static String ExtensionDependencyDetailsPage_ExtensionName;
+	public static String ExtensionDependencyDetailsPage_SpecifyExtensionDependencyDetails;
+	public static String FileField_Browse;
+	public static String OverviewPage_AddPath;
+	public static String OverviewPage_Appdir;
+	public static String OverviewPage_ChangePath;
+	public static String OverviewPage_CreatingDeploymentScript;
+	public static String OverviewPage_DeploymentScripts;
+	public static String OverviewPage_Description;
+	public static String OverviewPage_Docroot;
+	public static String OverviewPage_Doubleclick;
+	public static String OverviewPage_Exporting;
+	public static String OverviewPage_GeneralInfo;
+	public static String OverviewPage_GeneralInfoDescr;
+	public static String OverviewPage_Icon;
+	public static String OverviewPage_LaunchingAndDebuggingPHPApp;
+	public static String OverviewPage_LaunchingPHPApp;
+	public static String OverviewPage_Name;
+	public static String OverviewPage_OpenScript;
+	public static String OverviewPage_Overview;
+	public static String OverviewPage_PackageAndExport;
+	public static String OverviewPage_PersistentResources;
+	public static String OverviewPage_PersistentResourcesDescription;
+	public static String OverviewPage_ScriptsDescription;
+	public static String OverviewPage_Scriptsdir;
+	public static String OverviewPage_SelectedScriptDoesntExist;
+	public static String OverviewPage_SelectPath;
+	public static String OverviewPage_Summary;
+	public static String OverviewPage_Testing;
+	public static String OverviewPage_TestingDescr;
+	public static String ParameterDetailsPage_DefaultValue;
+	public static String ParameterDetailsPage_Description;
+	public static String ParameterDetailsPage_Display;
+	public static String ParameterDetailsPage_Id;
+	public static String ParameterDetailsPage_Identical;
+	public static String ParameterDetailsPage_ParamDetails;
+	public static String ParameterDetailsPage_Readonly;
+	public static String ParameterDetailsPage_Required;
+	public static String ParameterDetailsPage_Type;
+	public static String ParameterDetailsPage_ValidValues;
+	public static String ParametersMasterDetailsProvider_0;
+	public static String ParametersMasterDetailsProvider_newParamName;
+	public static String PHPDependencyDetailsPage_PHPDepDetails;
+	public static String PHPDependencyDetailsPage_Version;
+	public static String ResourceListSection_Add;
+	public static String ResourceListSection_Edit;
+	public static String ResourceListSection_Remove;
+	public static String ScriptsContentProvider_Activation;
+	public static String ScriptsContentProvider_Deactivation;
+	public static String ScriptsContentProvider_Staging;
+	public static String ScriptsContentProvider_Unstaging;
+	public static String ScriptsTreeSection_Info;
+	public static String ScriptsTreeSection_ScriptsMapping;
+	public static String VariableDetailsPage_Details;
+	public static String VariableDetailsPage_Name;
+	public static String VariableDetailsPage_Value;
+	public static String VariablesMasterDetailsProvider_DefaultVariableName;
+	public static String VariablesMasterDetailsProvider_VariablesDescr;
+	public static String VersionControl_Conflicts;
+	public static String VersionControl_Equals;
+	public static String VersionControl_Exclude;
+	public static String VersionControl_Maximum;
+	public static String VersionControl_Minimum;
+	public static String VersionControl_Range;
+	public static String ZendComponentDependencyDetailsPage_Details;
+	public static String ZendComponentDependencyDetailsPage_Name;
+	public static String ZendComponentDependencyDetailsPage_SpecifyDetails;
+	public static String ZendFrameworkDependencyDetailsPage_ZFDepDetails;
+	public static String ZendFrameworkDependencyDetailsPage_ZFDepVersion;
+	public static String ZendServerDependencyDetailsPage_Details;
+	public static String ZendServerDependencyDetailsPage_Version;
+	static {
+		// initialize resource bundle
+		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
+	}
+
+	private Messages() {
+	}
+}
