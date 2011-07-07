ec85a2bbd4a8528bc01de51b31713ee94f98c0e6
Jacek Pospychala <jacek.pospychala>
2011-07-07 12:30:16 +0000

    add validation markers

--- zendserver-deployment-eclipse/org.zend.php.zendserver.deployment.core/src/org/zend/php/zendserver/deployment/core/IncrementalDeploymentBuilder.java
+++ zendserver-deployment-eclipse/org.zend.php.zendserver.deployment.core/src/org/zend/php/zendserver/deployment/core/IncrementalDeploymentBuilder.java
@@ -4,6 +4,7 @@ import java.util.Map;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IMarker;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IResourceDelta;
@@ -13,12 +14,13 @@ import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.zend.php.zendserver.deployment.core.descriptor.DescriptorContainerManager;
 import org.zend.php.zendserver.deployment.core.descriptor.IDescriptorContainer;
+import org.zend.php.zendserver.deployment.core.internal.validation.ValidationStatus;
 import org.zend.php.zendserver.deployment.core.internal.validation.Validator;
 
 public class IncrementalDeploymentBuilder extends IncrementalProjectBuilder {
 
-	public static final String ID = DeploymentCore.PLUGIN_ID
-			+ ".DeploymentBuilder"; //$NON-NLS-1$
+	private static final String PROBLEM_MARKER = "org.zend.php.zendserver.deployment.core.problemmarker";
+	public static final String ID = DeploymentCore.PLUGIN_ID + ".DeploymentBuilder"; //$NON-NLS-1$
 
 	public IncrementalDeploymentBuilder() {
 		// TODO Auto-generated constructor stub
@@ -31,43 +33,56 @@ public class IncrementalDeploymentBuilder extends IncrementalProjectBuilder {
 		if (delta == null) {
 			return null;
 		}
-
+		
 		delta.accept(new IResourceDeltaVisitor() {
-
+			
 			public boolean visit(IResourceDelta delta) throws CoreException {
 				IResource resource = delta.getResource();
-				if ((resource instanceof IFile)
-						&& (DescriptorContainerManager.DESCRIPTOR_PATH
-								.equals(resource.getName()))) {
-					validateDescriptor((IFile) resource);
+				if ((resource instanceof IFile) && (DescriptorContainerManager.DESCRIPTOR_PATH.equals(resource.getName()))) {
+					validateDescriptor((IFile)resource);
 				}
-
+				
 				if (resource instanceof IProject) {
 					return true;
 				}
-
+				
 				if (resource instanceof IFolder) {
 					return false;
 				}
 				return false;
 			}
 		});
-
+		
 		return null;
 	}
 
 	protected void validateDescriptor(IFile file) {
+		IDescriptorContainer model = DescriptorContainerManager.getService()
+				.openDescriptorContainer(file);
+		
+		Validator validator = new Validator();
+		ValidationStatus[] statuses = validator.validate(model.getDescriptorModel());
+		
 		try {
-			IDescriptorContainer model = DescriptorContainerManager
-					.getService().openDescriptorContainer(file);
-
-			Validator validator = new Validator();
-			validator.validate(model.getDescriptorModel());
-			// TODO Add markers
-
-		} catch (Exception e) {
-			// log any exception but please don't prompt for any error
+			file.deleteMarkers(PROBLEM_MARKER, true, IResource.DEPTH_ZERO);
+		} catch (CoreException e) {
 			DeploymentCore.log(e);
+		}
+		
+		for (ValidationStatus status : statuses) {
+			IMarker marker;
+			try {
+				marker = file.createMarker(PROBLEM_MARKER);
+				marker.setAttribute("lineNumber", 0);
+				marker.setAttribute("charStart", 0);
+				marker.setAttribute("charEnd", 0);
+				marker.setAttribute("message", status.getMessage());
+				marker.setAttribute("priority", 0);
+				marker.setAttribute("severity", status.getSeverity());
+			} catch (CoreException e) {
+				DeploymentCore.log(e);
+			}
+			
 		}
 	}
 
