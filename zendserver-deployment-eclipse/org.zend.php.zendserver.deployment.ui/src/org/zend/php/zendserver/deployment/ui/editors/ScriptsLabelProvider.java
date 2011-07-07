b98cb7263b93d2a41ba048ba8694e04a18c98d10
Jacek Pospychala <jacek.pospychala>
2011-07-07 08:20:05 +0000

    working on scripts page

--- zendserver-deployment-eclipse/org.zend.php.zendserver.deployment.ui/src/org/zend/php/zendserver/deployment/ui/editors/ScriptsLabelProvider.java
+++ zendserver-deployment-eclipse/org.zend.php.zendserver.deployment.ui/src/org/zend/php/zendserver/deployment/ui/editors/ScriptsLabelProvider.java
@@ -8,41 +8,40 @@ import org.zend.php.zendserver.deployment.ui.editors.ScriptsContentProvider.Scri
 
 public class ScriptsLabelProvider extends LabelProvider {
 
-	private OverviewPage page;
+	private ScriptsSection section;
 
-	public ScriptsLabelProvider(OverviewPage page) {
-		this.page = page;
+	public ScriptsLabelProvider(ScriptsSection section) {
+		this.section = section;
 	}
-
+	
 	@Override
 	public String getText(Object element) {
 		if (element instanceof ScriptsContentProvider.ScriptType) {
 			return ((ScriptsContentProvider.ScriptType) element).name;
 		}
-
+		
 		if (element instanceof ScriptsContentProvider.Script) {
 			return ((ScriptsContentProvider.Script) element).name;
 		}
-
+		
 		return super.getText(element);
 	}
-
+	
 	@Override
 	public Image getImage(Object element) {
 		if (element instanceof ScriptsContentProvider.ScriptType) {
 			return Activator.getDefault().getImage(Activator.IMAGE_SCRIPT_TYPE);
-
+		
 		} else if (element instanceof ScriptsContentProvider.Script) {
 			ScriptsContentProvider.Script script = (Script) element;
-			IFile file = page.getScript(script.name);
+			IFile file = section.getScript(script.name);
 			if (file != null && file.exists()) {
 				return Activator.getDefault().getImage(Activator.IMAGE_SCRIPT);
 			} else {
-				return Activator.getDefault().getImage(
-						Activator.IMAGE_SCRIPT_NOTEXISTS);
+				return Activator.getDefault().getImage(Activator.IMAGE_SCRIPT_NOTEXISTS);
 			}
 		}
-
+		
 		return super.getImage(element);
 	}
 }
