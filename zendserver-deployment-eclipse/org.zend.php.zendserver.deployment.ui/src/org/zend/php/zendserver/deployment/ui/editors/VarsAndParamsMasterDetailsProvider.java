120168138d164f3000dfe0831e413b37017d302f
Jacek Pospychala <jacek.pospychala>
2011-07-08 08:47:13 +0000

    remove top-level grouping in tree viewer

--- zendserver-deployment-eclipse/org.zend.php.zendserver.deployment.ui/src/org/zend/php/zendserver/deployment/ui/editors/VarsAndParamsMasterDetailsProvider.java
+++ zendserver-deployment-eclipse/org.zend.php.zendserver.deployment.ui/src/org/zend/php/zendserver/deployment/ui/editors/VarsAndParamsMasterDetailsProvider.java
@@ -1,38 +1,16 @@
 package org.zend.php.zendserver.deployment.ui.editors;
 
-import java.util.List;
-
 import org.eclipse.jface.viewers.IStructuredContentProvider;
 import org.eclipse.jface.window.Window;
 import org.eclipse.ui.dialogs.ListDialog;
-import org.zend.php.zendserver.deployment.core.descriptor.DeploymentDescriptorPackage;
 import org.zend.php.zendserver.deployment.core.descriptor.IDeploymentDescriptor;
-import org.zend.php.zendserver.deployment.core.descriptor.IModelContainer;
 import org.zend.php.zendserver.deployment.core.descriptor.IParameter;
 import org.zend.php.zendserver.deployment.core.descriptor.IVariable;
-import org.zend.php.zendserver.deployment.core.internal.descriptor.Feature;
 import org.zend.php.zendserver.deployment.core.internal.descriptor.Parameter;
 import org.zend.php.zendserver.deployment.core.internal.descriptor.Variable;
 import org.zend.php.zendserver.deployment.ui.Messages;
 
 public class VarsAndParamsMasterDetailsProvider implements MasterDetailsProvider {
-
-	private class Node {
-		public String label;
-		private Feature feature;
-		private IModelContainer container;
-		
-		public Node(String label, Feature feature, IModelContainer container) {
-			this.label = label;
-			this.feature = feature;
-			this.container = container;
-		}
-		
-		@Override
-		public String toString() {
-			return label;
-		}
-	}
 
 	private static final Object[] EMPTY = new Object[0];
 	
@@ -40,31 +18,17 @@ public class VarsAndParamsMasterDetailsProvider implements MasterDetailsProvider
 		return Messages.VarsAndParamsMasterDetailsProvider_DefineVarsAndParams;
 	}
 	
-	private Node[] rootNodes;
-	
 	public Object[] doGetElements(Object input) {
 		if (input instanceof IDeploymentDescriptor) {
 			IDeploymentDescriptor descr = (IDeploymentDescriptor) input;
-			List<IVariable> vars = descr.getVariables();
-			List<IParameter> params = descr.getParameters();
-			if (vars.size() > 0 && params.size() > 0) {
-				if (rootNodes == null) {
-					rootNodes = new Node[] {
-							new Node(Messages.VarsAndParamsMasterDetailsProvider_1, DeploymentDescriptorPackage.VARIABLES, (IModelContainer)input),
-							new Node(Messages.VarsAndParamsMasterDetailsProvider_2, DeploymentDescriptorPackage.PARAMETERS, (IModelContainer)input)
-					};
-				}
-				return rootNodes;
-			} else if (vars.size() > 0) {
-				return vars.toArray();
-			} else if (params.size() > 0) {
-				return params.toArray();
-			}
-		}
-		
-		if (input instanceof Node) {
-			Node node = (Node) input;
-			return node.container.getChildren(node.feature).toArray();
+			Object[] vars = descr.getVariables().toArray();
+			Object[] params = descr.getParameters().toArray();
+			
+			Object[] result = new Object[vars.length + params.length];
+			System.arraycopy(vars,  0, result, 0, vars.length);
+			System.arraycopy(params,  0, result, vars.length, params.length);
+			
+			return result;
 		}
 		
 		if (input instanceof Object[]) {
@@ -102,13 +66,6 @@ public class VarsAndParamsMasterDetailsProvider implements MasterDetailsProvider
 	}
 
 	public Object doGetParent(Object element) {
-		if (rootNodes != null) {
-			if (element instanceof IVariable) {
-				return rootNodes[0];
-			} else if (element instanceof IParameter) {
-				return rootNodes[1];
-			}
-		}
 		return null;
 	}
 }
