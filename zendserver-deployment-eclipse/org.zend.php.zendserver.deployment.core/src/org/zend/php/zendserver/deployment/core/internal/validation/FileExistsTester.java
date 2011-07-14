0111971d0a9414959de34bc50dc3c3538a14183a
ganoro@gmail.com <ganoro@gmail.com>
2011-07-14 08:21:24 +0000

    small fix to equls

--- zendserver-deployment-eclipse/org.zend.php.zendserver.deployment.core/src/org/zend/php/zendserver/deployment/core/internal/validation/FileExistsTester.java
+++ zendserver-deployment-eclipse/org.zend.php.zendserver.deployment.core/src/org/zend/php/zendserver/deployment/core/internal/validation/FileExistsTester.java
@@ -2,8 +2,6 @@ package org.zend.php.zendserver.deployment.core.internal.validation;
 
 import java.io.File;
 
-import org.zend.php.zendserver.deployment.core.internal.descriptor.Feature;
-
 public class FileExistsTester extends PropertyTester {
 
 	public FileExistsTester() {
@@ -17,7 +15,7 @@ public class FileExistsTester extends PropertyTester {
 		}
 		if (value instanceof String) {
 			String s = (String) value;
-			if ("".equals(s.trim())) {
+			if (s.trim().length() == 0) {
 				return null;
 			}
 			File f = new File(s);
