3204e22b087e2ca260374a38329b5dfa8d535357
Wojciech Galanciak <wojciech.galanciak@gmail.com>
2011-07-18 14:39:27 +0000

    Add deployment launch config attributes

---
+++ zendserver-deployment-eclipse/org.zend.php.zendserver.deployment.core/src/org/zend/php/zendserver/deployment/core/debugger/DeploymentAttributes.java
@@ -0,0 +1,31 @@
+package org.zend.php.zendserver.deployment.core.debugger;
+
+public enum DeploymentAttributes {
+
+	BASE_PATH("basePath"), //$NON-NLS-1$
+	
+	TARGET_ID("targetId"), //$NON-NLS-1$
+	
+	PROJECT_NAME("projectName"), //$NON-NLS-1$
+	
+	PARAMETERS_PATH("parametersPath"), //$NON-NLS-1$
+	
+	APPLICATION_NAME("applicationName"), //$NON-NLS-1$
+	
+	IGNORE_FAILURES("ignoreFailures"), //$NON-NLS-1$
+	
+	DEFAULT_SERVER("defaultServer"), //$NON-NLS-1$
+	
+	VIRTUAL_HOST("virtualHost"); //$NON-NLS-1$
+	
+	private String name;
+	
+	private DeploymentAttributes(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+}
