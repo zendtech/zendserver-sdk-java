b6221445a0bf54b5a68494aabb4ae4588166b179
Jacek Pospychala <jacek.pospychala>
2011-07-18 11:07:17 +0000

    remove wrong javadoc

--- zendserver-deployment-eclipse/org.zend.php.zendserver.deployment.ui/src/org/zend/php/zendserver/deployment/ui/editors/DeploymentDescriptorEditor.java
+++ zendserver-deployment-eclipse/org.zend.php.zendserver.deployment.ui/src/org/zend/php/zendserver/deployment/ui/editors/DeploymentDescriptorEditor.java
@@ -42,15 +42,6 @@ import org.zend.php.zendserver.deployment.ui.Activator;
 import org.zend.php.zendserver.deployment.ui.Messages;
 import org.zend.sdklib.mapping.MappingModelFactory;
 
-/**
- * An example showing how to create a multi-page editor. This example has 3
- * pages:
- * <ul>
- * <li>page 0 contains a nested text editor.
- * <li>page 1 allows you to change the font used in page 2
- * <li>page 2 shows the words in page 0 in sorted order
- * </ul>
- */
 public class DeploymentDescriptorEditor extends FormEditor implements
 		IResourceChangeListener {
 
