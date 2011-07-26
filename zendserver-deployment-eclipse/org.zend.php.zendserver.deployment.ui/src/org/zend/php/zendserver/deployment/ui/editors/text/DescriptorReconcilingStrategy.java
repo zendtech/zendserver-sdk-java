9c4e46e81b3af6388ef88057aebce8db90d20903
Jacek Pospychala <jacek.pospychala>
2011-07-26 14:35:12 +0000

    ZSTD-553 add as-you-type validation in descriptor editor

--- zendserver-deployment-eclipse/org.zend.php.zendserver.deployment.ui/src/org/zend/php/zendserver/deployment/ui/editors/text/DescriptorReconcilingStrategy.java
+++ zendserver-deployment-eclipse/org.zend.php.zendserver.deployment.ui/src/org/zend/php/zendserver/deployment/ui/editors/text/DescriptorReconcilingStrategy.java
@@ -5,12 +5,16 @@ import org.eclipse.jface.text.IRegion;
 import org.eclipse.jface.text.reconciler.DirtyRegion;
 import org.eclipse.jface.text.reconciler.IReconcilingStrategy;
 import org.zend.php.zendserver.deployment.core.descriptor.IDescriptorContainer;
+import org.zend.php.zendserver.deployment.core.internal.validation.DescriptorSemanticValidator;
+import org.zend.php.zendserver.deployment.core.internal.validation.DescriptorValidator;
+import org.zend.php.zendserver.deployment.core.internal.validation.ValidationStatus;
 import org.zend.php.zendserver.deployment.ui.editors.DeploymentDescriptorEditor;
 
 
 public class DescriptorReconcilingStrategy implements IReconcilingStrategy {
 
 	private DeploymentDescriptorEditor fEditor;
+	private DescriptorSemanticValidator validator = new DescriptorSemanticValidator();
 
 	public DescriptorReconcilingStrategy(DeploymentDescriptorEditor editor) {
 		this.fEditor = editor;
@@ -26,6 +30,9 @@ public class DescriptorReconcilingStrategy implements IReconcilingStrategy {
 	public void reconcile(IRegion partition) {
 		IDescriptorContainer container = fEditor.getDescriptorContainer();
 		container.load();
+		
+		ValidationStatus[] statuses = validator.validate(container.getDescriptorModel(), fEditor.getDocument());
+		DescriptorValidator.reportProblems(container.getFile(), statuses);
 	}
 
 }
