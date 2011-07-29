e7035f77a005255ac0e8561b53e7786e916058fe
Jacek Pospychala <jacek.pospychala>
2011-07-29 08:25:42 +0000

    ZSTD-570 add drag'n'drop support in targets view

--- zendserver-deployment-eclipse/org.zend.php.zendserver.deployment.ui/src/org/zend/php/zendserver/deployment/ui/targets/DropTransferListener.java
+++ zendserver-deployment-eclipse/org.zend.php.zendserver.deployment.ui/src/org/zend/php/zendserver/deployment/ui/targets/DropTransferListener.java
@@ -1,5 +1,9 @@
 package org.zend.php.zendserver.deployment.ui.targets;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.jface.action.ContributionManager;
 import org.eclipse.jface.action.GroupMarker;
@@ -13,11 +17,16 @@ import org.eclipse.swt.widgets.Menu;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.menus.IMenuService;
 import org.eclipse.ui.part.ResourceTransfer;
+import org.zend.sdklib.target.IZendTarget;
 
 public class DropTransferListener extends ViewerDropAdapter {
 
+	private static final String PROJECT_NAME = "projectName"; //$NON-NLS-1$
+	private static final String TARGET_ID = "targetId"; //$NON-NLS-1$
+	
 	private static final String DROP_MENU = "dropMenu"; //$NON-NLS-1$
 	private static final String MENU_LOCATION_URI = "menu:org.zend.php.zendserver.deployment.ui.targets.TargetsViewer"; //$NON-NLS-1$
+
 	private Viewer viewer;
 	
 	protected DropTransferListener(Viewer viewer) {
@@ -27,7 +36,19 @@ public class DropTransferListener extends ViewerDropAdapter {
 
 	@Override
 	public boolean performDrop(Object data) {
-		IResource[] resources = (IResource[]) data; // TODO pass resources and selected target to command
+		IResource[] resources = (IResource[]) data;
+		List<String> projectNames = new ArrayList<String>();
+		for (IResource res : resources) {
+			if (res instanceof IProject) {
+				projectNames.add(res.getName());
+			}
+		}
+		if (projectNames.size() == 0) {
+			return false;
+		}
+		
+		// can cast safely, after type check in #validateDrop()
+		IZendTarget target = (IZendTarget) getCurrentTarget();
 		
 		Display display = Display.getDefault();
 		final Menu menu = new Menu(viewer.getControl().getShell(), SWT.POP_UP);
@@ -41,7 +62,14 @@ public class DropTransferListener extends ViewerDropAdapter {
 			}
 		};
 		cm.add(new GroupMarker(DROP_MENU));
+		
 		IMenuService service = (IMenuService) PlatformUI.getWorkbench().getService(IMenuService.class);
+		
+		// pass selected objects and drop target as a context
+		Object projName = projectNames.size() == 1 ?  projectNames.get(0) : projectNames.toArray(new String[projectNames.size()]);
+		service.getCurrentState().addVariable(PROJECT_NAME, projName);
+		service.getCurrentState().addVariable(TARGET_ID, target.getId());
+		
 		service.populateContributionManager(cm, MENU_LOCATION_URI);
 		cm.update(false);
 		
@@ -59,7 +87,10 @@ public class DropTransferListener extends ViewerDropAdapter {
 	@Override
 	public boolean validateDrop(Object target, int operation,
 			TransferData type) {
-		return ResourceTransfer.getInstance().isSupportedType(type);
+		boolean isSupported = ResourceTransfer.getInstance().isSupportedType(type);
+		boolean isZendTarget = target instanceof IZendTarget;
+		
+		return isSupported && isZendTarget;
 	}
 
 }
