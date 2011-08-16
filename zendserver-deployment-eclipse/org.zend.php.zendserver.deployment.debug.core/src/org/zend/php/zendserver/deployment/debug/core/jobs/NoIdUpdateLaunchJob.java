ec6116b162c583cf795b03179e8eb3e3df74277d
ganoro@gmail.com <ganoro@gmail.com>
2011-08-16 19:10:19 +0000

    when comparing application urls, ignore protocol.  This is useful when hosts manage their own protocol (http/https)

--- zendserver-deployment-eclipse/org.zend.php.zendserver.deployment.debug.core/src/org/zend/php/zendserver/deployment/debug/core/jobs/NoIdUpdateLaunchJob.java
+++ zendserver-deployment-eclipse/org.zend.php.zendserver.deployment.debug.core/src/org/zend/php/zendserver/deployment/debug/core/jobs/NoIdUpdateLaunchJob.java
@@ -5,6 +5,8 @@ import java.net.URL;
 import java.util.List;
 
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
 import org.zend.php.zendserver.deployment.debug.core.Messages;
 import org.zend.php.zendserver.deployment.debug.core.config.IDeploymentHelper;
 import org.zend.sdklib.application.ZendApplication;
@@ -18,7 +20,8 @@ public class NoIdUpdateLaunchJob extends DeploymentLaunchJob {
 	}
 
 	@Override
-	protected ApplicationInfo performOperation(ZendApplication app, String projectPath) {
+	protected ApplicationInfo performOperation(ZendApplication app,
+			String projectPath) {
 		ApplicationsList list = app.getStatus(helper.getTargetId());
 		List<ApplicationInfo> infos = list.getApplicationsInfo();
 		String appId = null;
@@ -26,23 +29,30 @@ public class NoIdUpdateLaunchJob extends DeploymentLaunchJob {
 			URL url = helper.getBaseURL();
 			if (helper.isDefaultServer()) {
 				try {
-					url = new URL(url.getProtocol(), IDeploymentHelper.DEFAULT_SERVER,
-							url.getFile());
+					url = new URL(url.getProtocol(),
+							IDeploymentHelper.DEFAULT_SERVER, url.getFile());
 				} catch (MalformedURLException e) {
 					// ignore
 				}
 			}
-			String urlString = url.toString();
+			IPath urlPath = new Path(url.toString());
+			final int segmentCount = urlPath.segmentCount();
+
 			for (ApplicationInfo info : infos) {
-				if (info.getBaseUrl().equals(urlString)) {
+				final IPath baseUrl = new Path(info.getBaseUrl());
+				// ignore the device (protocol) when comparing urls, this is
+				// useful because servers can manage their
+				// own protocols in different ways
+				if (baseUrl.matchingFirstSegments(urlPath) == segmentCount) {
 					appId = String.valueOf(info.getId());
 					break;
 				}
 			}
 		}
 		if (appId != null) {
-			return app.update(project.getLocation().toString(), helper.getTargetId(), appId,
-					helper.getUserParams(), helper.isIgnoreFailures());
+			return app.update(project.getLocation().toString(),
+					helper.getTargetId(), appId, helper.getUserParams(),
+					helper.isIgnoreFailures());
 		}
 		// should not happen because the same base URL is already used
 		return null;
