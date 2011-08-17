c43a43f5c3ba770ea459b15100a0e1d8bb55e06a
Jacek Pospychala <jacek.pospychala>
2011-08-17 11:36:11 +0000

    ZSTD-661 double-click on launch config in targets view should open deployment wizard

--- zendserver-deployment-eclipse/org.zend.php.zendserver.deployment.debug.ui/src/org/zend/php/zendserver/deployment/debug/ui/listeners/LaunchConfigurationDoubleClickListener.java
+++ zendserver-deployment-eclipse/org.zend.php.zendserver.deployment.debug.ui/src/org/zend/php/zendserver/deployment/debug/ui/listeners/LaunchConfigurationDoubleClickListener.java
@@ -1,21 +1,12 @@
 package org.zend.php.zendserver.deployment.debug.ui.listeners;
 
-import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.ResourcesPlugin;
-import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.debug.core.ILaunchConfiguration;
+import org.eclipse.debug.ui.DebugUITools;
 import org.eclipse.jface.viewers.DoubleClickEvent;
 import org.eclipse.jface.viewers.IDoubleClickListener;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.jface.window.Window;
-import org.eclipse.jface.wizard.WizardDialog;
 import org.eclipse.swt.widgets.Shell;
-import org.eclipse.ui.PlatformUI;
-import org.zend.php.zendserver.deployment.debug.core.config.IDeploymentHelper;
-import org.zend.php.zendserver.deployment.debug.core.jobs.DeployLaunchJob;
-import org.zend.php.zendserver.deployment.debug.core.jobs.UpdateLaunchJob;
-import org.zend.php.zendserver.deployment.debug.ui.wizards.DeploymentWizard;
 
 public class LaunchConfigurationDoubleClickListener implements IDoubleClickListener {
 
@@ -25,28 +16,9 @@ public class LaunchConfigurationDoubleClickListener implements IDoubleClickListe
 		Object obj = sselection.getFirstElement();
 		ILaunchConfiguration config = (ILaunchConfiguration) obj;
 		
-		final DeploymentWizard wizard = new DeploymentWizard(config);
-		Shell shell = PlatformUI.getWorkbench().getDisplay().getActiveShell();
-		WizardDialog dialog = new WizardDialog(shell, wizard);
-		dialog.setPageSize(550, 350);
-		dialog.create();
-		if (dialog.open() == Window.OK) {
-			Job job;
-			IDeploymentHelper updatedHelper = wizard.getHelper();
-			IProject project = ResourcesPlugin.getWorkspace().getRoot().getProject(updatedHelper.getProjectName());
-			switch (updatedHelper.getOperationType()) {
-			case IDeploymentHelper.DEPLOY:
-				job = new DeployLaunchJob(updatedHelper, project);
-				break;
-			case IDeploymentHelper.UPDATE:
-				job = new UpdateLaunchJob(updatedHelper, project);
-				break;
-			default:
-				return;
-			}
-			job.setUser(true);
-			job.schedule();
-		}
+		Shell shell = event.getViewer().getControl().getShell();
+		
+		DebugUITools.openLaunchConfigurationPropertiesDialog(shell, config, "org.eclipse.debug.ui.launchGroup.run"); //$NON-NLS-1$
 	}
 	
 }
