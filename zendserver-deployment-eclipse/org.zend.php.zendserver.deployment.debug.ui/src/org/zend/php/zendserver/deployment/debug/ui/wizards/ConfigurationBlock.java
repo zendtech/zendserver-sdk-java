fc9eda8ceab03458517b4a9fc92a633bd1bfa471
Wojciech Galanciak <wojciech.galanciak@gmail.com>
2011-08-17 14:50:49 +0000

    Advanced Settings are not expanded correctly in Launch Configuration Dialog

--- zendserver-deployment-eclipse/org.zend.php.zendserver.deployment.debug.ui/src/org/zend/php/zendserver/deployment/debug/ui/wizards/ConfigurationBlock.java
+++ zendserver-deployment-eclipse/org.zend.php.zendserver.deployment.debug.ui/src/org/zend/php/zendserver/deployment/debug/ui/wizards/ConfigurationBlock.java
@@ -96,6 +96,7 @@ public class ConfigurationBlock extends AbstractBlock {
 					Point point = shell.computeSize(SWT.DEFAULT, SWT.DEFAULT, true);
 					shell.setSize(shell.getSize().x, point.y);
 				}
+				getContainer().pack();
 			}
 		});
 		expComposite.setText(Messages.advancedSection_Title);
