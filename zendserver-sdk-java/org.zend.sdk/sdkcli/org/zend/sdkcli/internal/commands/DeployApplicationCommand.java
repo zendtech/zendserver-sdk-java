b0c8ed8f515122d969ff5cae6d67ac3281f00c04
ganoro@gmail.com <ganoro@gmail.com@b75f36dc-5d77-e969-e1c6-c1f8c8b91a4e>
2011-08-25 06:36:20 +0000

    ZSTD-668 [Zend SDK] Remove the -d option from deploy application command
    fixed

--- zendserver-sdk-java/org.zend.sdk/sdkcli/org/zend/sdkcli/internal/commands/DeployApplicationCommand.java
+++ zendserver-sdk-java/org.zend.sdk/sdkcli/org/zend/sdkcli/internal/commands/DeployApplicationCommand.java
@@ -26,7 +26,6 @@ public class DeployApplicationCommand extends ApplicationAwareCommand {
 	private static final String NAME = "n";
 	private static final String IGNORE_FAILURES = "f";
 	private static final String VHOST = "h";
-	private static final String DEFAULT_SERVER = "d";
 
 	@Option(opt = PATH, required = false, description = "Application package location or project's directory, if not provided current directory is considered", argName = "path")
 	public String getPath() {
@@ -57,37 +56,6 @@ public class DeployApplicationCommand extends ApplicationAwareCommand {
 		return hasOption(IGNORE_FAILURES);
 	}
 
-	/**
-	 * Validates that both create + default are not present together. If none is
-	 * chosen then the default option should be considered. Else just return the
-	 * one that is turned on.
-	 * 
-	 * @return true iff default server
-	 */
-	@Option(opt = DEFAULT_SERVER, required = false, description = "Use default server for this application")
-	public boolean isDefaultServer() {
-		final boolean defaultServer = hasOption(DEFAULT_SERVER);
-		final boolean vhost = isVhost();
-
-		// both turned on -> error
-		if (defaultServer && vhost) {
-			final IllegalArgumentException e = new IllegalArgumentException(
-					"Error: both create host and default "
-							+ "server options are provided. Only one option "
-							+ "should be present at a time.");
-			getLogger().error(e);
-			throw e;
-		}
-
-		// both turned off -> default
-		if (!defaultServer && !vhost) {
-			return true;
-		}
-
-		// only one is enabled
-		return defaultServer;
-	}
-
 	@Option(opt = VHOST, required = false, description = "The name of the vhost to create or use if exists")
 	public String getVhost() {
 		return getValue(VHOST);
@@ -101,7 +69,7 @@ public class DeployApplicationCommand extends ApplicationAwareCommand {
 	public boolean doExecute() {
 		ApplicationInfo info = getApplication().deploy(getPath(),
 				getBasePath(), getTargetId(), getParams(), getName(),
-				isIgnoreFailures(), getVhost(), isDefaultServer());
+				isIgnoreFailures(), getVhost(), !isVhost());
 		if (info == null) {
 			return false;
 		}
