d17c2959c6fa24caa348154cc78678e1ca9d1fd4
jacek.pospychala <jacek.pospychala@b75f36dc-5d77-e969-e1c6-c1f8c8b91a4e>
2011-09-01 12:55:48 +0000

    ZSTD-679	 Improve testing target status

--- org.zend.sdk/sdklib/org/zend/sdklib/manager/PrivilegesException.java
+++ org.zend.sdk/sdklib/org/zend/sdklib/manager/PrivilegesException.java
@@ -2,6 +2,10 @@ package org.zend.sdklib.manager;
 
 public class PrivilegesException extends DetectionException {
 
+	public PrivilegesException(String message) {
+		super(message);
+	}
+
 	private static final long serialVersionUID = 4641097690385954184L;
 
 }
