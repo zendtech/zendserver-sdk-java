c2f52bd7cb31ca90f9d1e901e206cb69523decba
ganoro@gmail.com <ganoro@gmail.com>
2011-09-05 16:52:42 +0000

    ZSTD-724 Web browser doesn't open when using a non-deployment project + run-config pops errors

--- zendserver-deployment-eclipse/org.zend.php.zendserver.deployment.debug.ui/src/org/zend/php/zendserver/deployment/debug/ui/listeners/DeploymentLaunchListener.java
+++ zendserver-deployment-eclipse/org.zend.php.zendserver.deployment.debug.ui/src/org/zend/php/zendserver/deployment/debug/ui/listeners/DeploymentLaunchListener.java
@@ -1,17 +1,32 @@
 package org.zend.php.zendserver.deployment.debug.ui.listeners;
 
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.php.debug.core.debugger.launching.ILaunchDelegateListener;
+import org.zend.php.zendserver.deployment.core.DeploymentNature;
+import org.zend.php.zendserver.deployment.debug.core.config.LaunchUtils;
+import org.zend.php.zendserver.deployment.debug.ui.Activator;
 import org.zend.php.zendserver.deployment.debug.ui.config.DeploymentHandler;
 
 public class DeploymentLaunchListener implements ILaunchDelegateListener {
 
-	public int preLaunch(ILaunchConfiguration configuration, String mode, ILaunch launch,
-			IProgressMonitor monitor) {
-		DeploymentHandler handler = new DeploymentHandler(configuration);
-		return handler.executeDeployment(mode);
+	public int preLaunch(ILaunchConfiguration configuration, String mode,
+			ILaunch launch, IProgressMonitor monitor) {
+		try {
+			final IProject project = LaunchUtils.getProjectFromFilename(configuration);
+			if (project == null || !project.hasNature(DeploymentNature.ID)) {
+				 return 0;
+			}
+			
+			DeploymentHandler handler = new DeploymentHandler(configuration);
+			return handler.executeDeployment(mode);
+		} catch (CoreException e) {
+			Activator.log(e);
+		}
+		return 0;
 	}
-	
+
 }
