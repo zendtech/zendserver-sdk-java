51d5310432bbe6468d9a9197202ab0ce47e4ad6a
jacek.pospychala <jacek.pospychala@b75f36dc-5d77-e969-e1c6-c1f8c8b91a4e>
2011-09-12 10:34:02 +0000

    decrypt public key

---
+++ org.zend.sdk/sdklib/org/zend/sdklib/internal/target/PublicKeyBuilder.java
@@ -0,0 +1,22 @@
+package org.zend.sdklib.internal.target;
+
+import java.io.IOException;
+
+import org.zend.sdklib.SdkException;
+
+/**
+ * PublicKeyBuilder is capable of generating a public key for given private key
+ */
+public interface PublicKeyBuilder {
+
+	/**
+	 * Returns encoded public key string for provided private key file path
+	 *  
+	 * @param privateKeyPath
+	 * @return
+	 * @throws SdkException 
+	 * @throws IOException 
+	 */
+	String getPublicKey(String privateKeyPath) throws PublicKeyNotFoundException;
+	
+}
