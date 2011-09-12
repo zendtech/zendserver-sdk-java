a26e09d26ed7ccedd2e2217a18141ef1b6b4cb05
jacek.pospychala <jacek.pospychala@b75f36dc-5d77-e969-e1c6-c1f8c8b91a4e>
2011-09-12 05:58:22 +0000

    No public key handling

---
+++ org.zend.sdk/sdklib/org/zend/sdklib/internal/target/PublicKeyNotFoundException.java
@@ -0,0 +1,13 @@
+package org.zend.sdklib.internal.target;
+
+import org.zend.sdklib.SdkException;
+
+public class PublicKeyNotFoundException extends SdkException {
+
+	private static final long serialVersionUID = -6908314413137760828L;
+
+	public PublicKeyNotFoundException(String message) {
+		super(message);
+	}
+	
+}
