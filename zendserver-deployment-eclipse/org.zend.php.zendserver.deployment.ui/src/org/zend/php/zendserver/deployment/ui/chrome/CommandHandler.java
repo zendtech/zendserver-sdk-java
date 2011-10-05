ff7fa362444be0a7d19015f8c6bbcfeaf30f5290
Jacek Pospychala <jacek.pospychala>
2011-10-05 09:32:08 +0000

    replace jetty with http-like socket server

--- zendserver-deployment-eclipse/org.zend.php.zendserver.deployment.ui/src/org/zend/php/zendserver/deployment/ui/chrome/CommandHandler.java
+++ zendserver-deployment-eclipse/org.zend.php.zendserver.deployment.ui/src/org/zend/php/zendserver/deployment/ui/chrome/CommandHandler.java
@@ -3,11 +3,6 @@ package org.zend.php.zendserver.deployment.ui.chrome;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Map.Entry;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.eclipse.core.commands.Command;
 import org.eclipse.core.commands.ExecutionEvent;
@@ -18,7 +13,6 @@ import org.eclipse.core.commands.common.NotDefinedException;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.commands.ICommandService;
-import org.mortbay.jetty.handler.AbstractHandler;
 import org.mortbay.util.ajax.JSON;
 
 /**
@@ -32,31 +26,21 @@ import org.mortbay.util.ajax.JSON;
  * { status : "Error", message: "Unknown command" }
  * </pre>
  */
-public class CommandHandler extends AbstractHandler {
+public class CommandHandler {
 
 	private static final String STATUS = "status"; //$NON-NLS-1$
 	private static final String MESSAGE = "message"; //$NON-NLS-1$
 	private static final String STATUS_ERROR = "Error"; //$NON-NLS-1$
 	private static final String STATUS_SUCCESS = "Success"; //$NON-NLS-1$
 
-	public void handle(String target, HttpServletRequest request,
-			HttpServletResponse response, int dispatch) throws IOException,
-			ServletException {
+	public void handle(HttpRequest request, HttpResponse response) throws IOException {
 
 		Map params = request.getParameterMap();
-		Map stringsMap = new HashMap(params.size());
-		for (Object o : params.entrySet()) {
-			Map.Entry param = (Entry) o;
-			String[] strings = (String[]) param.getValue();
-			if ((strings != null) && (strings.length > 0)) {
-				stringsMap.put(param.getKey(), strings[0]);
-			}
-		}
 		
-		String path = request.getPathInfo();
+		String path = request.getPath();
 		String errorMessage = null;
 		try {
-			executeCommand(path, stringsMap);
+			executeCommand(path, params);
 		} catch (ExecutionException e) {
 			errorMessage = e.getMessage();
 		} catch (NotDefinedException e) {
@@ -68,7 +52,7 @@ public class CommandHandler extends AbstractHandler {
 		}
 
 		response.setContentType("application/json;charset=utf-8"); //$NON-NLS-1$
-		response.setStatus(HttpServletResponse.SC_OK);
+		response.setStatus(HttpResponse.OK);
 		
 		Map<String, String> result = new HashMap<String, String>();
 		result.put(STATUS, errorMessage == null ? STATUS_SUCCESS : STATUS_ERROR);
@@ -76,9 +60,7 @@ public class CommandHandler extends AbstractHandler {
 			result.put(MESSAGE, errorMessage);
 		}
 		String json = JSON.toString(result);
-		response.getWriter().println(json);
-		response.flushBuffer();
-
+		response.send(json);
 	}
 
 	private void executeCommand(String path, Map params) throws ExecutionException, NotDefinedException, NotEnabledException, NotHandledException {
--- zendserver-deployment-eclipse/org.zend.php.zendserver.deployment.ui/src/org/zend/php/zendserver/deployment/ui/chrome/SocketCommandListener.java
+++ zendserver-deployment-eclipse/org.zend.php.zendserver.deployment.ui/src/org/zend/php/zendserver/deployment/ui/chrome/SocketCommandListener.java
@@ -1,52 +1,150 @@
 package org.zend.php.zendserver.deployment.ui.chrome;
 
-import org.mortbay.jetty.Server;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.URISyntaxException;
 
+import org.zend.php.zendserver.deployment.ui.Activator;
 
 /**
- * Listens for requests on network socket and executes Eclipse commands.
- * Request to execute a command should contain command id in the request path last segment.
- * Parameters passed in request query will be provided in command execution event.
- * For example:
+ * Listens for requests on network socket and executes Eclipse commands. Request
+ * to execute a command should contain command id in the request path last
+ * segment. Parameters passed in request query will be provided in command
+ * execution event. For example:
  * 
- * http://127.0.0.1/org.zend.php.deployment.debug.OpenSshTunnel?container=myContainer
+ * http://127.0.0.1/org.zend.php.deployment.debug.OpenSshTunnel?container=
+ * myContainer
  * 
- * invokes org.zend.php.deploymentdebug.OpenSshTunnel with single param "container".
- *
+ * invokes org.zend.php.deploymentdebug.OpenSshTunnel with single param
+ * "container".
+ * 
  */
 public class SocketCommandListener {
-
-	private static final String SERVER_ID = "socketCommand"; //$NON-NLS-1$
 	
 	private Thread listeningThread;
 	private static final int port = 28029;
+	
+	private CommandHandler handler = new CommandHandler();
 
 	public void start() {
 		listeningThread = new Thread(new Runnable() {
 
 			public void run() {
-				Server server = new Server(port);
-				server.setHandler(new CommandHandler());
+				ServerSocket serverSocket = null;
 				try {
-					server.start();
-				} catch (Exception e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+					serverSocket = new ServerSocket(port);
+				} catch (IOException ex) {
+					Activator.log(ex);
+					return;
 				}
+
 				try {
-					server.join();
-				} catch (InterruptedException e) {
-					// quit
+					serverLoop(serverSocket);
+				} finally {
+					try {
+						serverSocket.close();
+					} catch (IOException ex) {
+						Activator.log(ex);
+					}
 				}
 			}
+
 			
 		});
 		listeningThread.start();
-		
-		
-		
+	}
+
+	private void serverLoop(ServerSocket serverSocket) {
+		while (true) {
+			Socket clientSocket = null;
+			BufferedReader in = null;
+			PrintWriter out = null;
+			try {
+				clientSocket = serverSocket.accept();
+				in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+				out = new PrintWriter(clientSocket.getOutputStream(), true);
+				handleClientSocket(in, out);
+				
+			} catch (IOException ex) {
+				Activator.log(ex);
+			} finally {
+				try {
+					if (in != null) {
+						in.close();
+					}
+					
+					if (out != null) {
+						out.close();
+					}
+					
+					if (clientSocket != null) {
+						clientSocket.close();
+					}
+				} catch (IOException ex) {
+					Activator.log(ex);
+				}
+			}
+		}
+	}
+
+	private void handleClientSocket(BufferedReader in, PrintWriter out) throws IOException {
+		HttpResponse response = new HttpResponse(out);
+		try {
+			HttpRequest request = readRequest(in);
+			handler.handle(request, response);
+		} catch (ParseError e) {
+			response.setStatus(HttpResponse.ERROR);
+			response.send(null);
+		}
 	}
 	
+	private HttpRequest readRequest(BufferedReader in) throws IOException, ParseError {
+		HttpRequest httpRequest = new HttpRequest();
+
+		try {
+			String request = in.readLine();
+			int idx = request.indexOf(' ');
+			if (idx == -1) {
+				throw new ParseError();
+			}
+			String method = request.substring(0, idx);
+			httpRequest.setMethod(method);
+			int idx2 = request.indexOf(' ', idx + 1);
+			if (idx2 == -1) {
+				throw new ParseError();
+			}
+			String requestPath = request.substring(idx + 1, idx2);
+			try {
+				httpRequest.setRequest(requestPath);
+			} catch (URISyntaxException e) {
+				throw new ParseError();
+			}
+			
+			String inputLine;
+			while (((inputLine = in.readLine()) != null) && (inputLine.length() > 0)) {
+				int colon = inputLine.indexOf(':');
+				if (colon == -1) {
+					throw new ParseError();
+				}
+				String key = inputLine.substring(0, colon);
+				String value = inputLine.substring(colon + 1).trim();
+				httpRequest.getHeaders().put(key, value);
+			}
+
+		} finally {
+			if (in != null) {
+				in.close();
+			}
+		}
+		
+		return httpRequest;
+
+	}
+
 	public void stop() {
 		if (listeningThread != null) {
 			listeningThread.interrupt();
