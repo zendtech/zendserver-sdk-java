3d72a60f77a4b8777b0485b4c9521c9a3bca5267
Wojciech Galanciak <wojciech.galanciak@gmail.com>
2011-10-13 22:23:42 +0000

    Add 'Open SSH Tunnel' action to targets view ZSTD-951 Incorrect icon for 'Deploy PHP Application' in Targets view

--- zendserver-deployment-eclipse/org.zend.php.zendserver.deployment.debug.ui/src/org/zend/php/zendserver/deployment/debug/ui/commands/OpenTunnelCommand.java
+++ zendserver-deployment-eclipse/org.zend.php.zendserver.deployment.debug.ui/src/org/zend/php/zendserver/deployment/debug/ui/commands/OpenTunnelCommand.java
@@ -1,13 +1,18 @@
 package org.zend.php.zendserver.deployment.debug.ui.commands;
 
-import java.io.IOException;
+import java.util.List;
 
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.expressions.EvaluationContext;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.osgi.util.NLS;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.PlatformUI;
 import org.zend.php.zendserver.deployment.core.targets.TargetsManagerService;
 import org.zend.php.zendserver.deployment.debug.core.tunnel.ZendDevCloudTunnelManager;
+import org.zend.php.zendserver.deployment.debug.ui.Messages;
 import org.zend.sdklib.target.IZendTarget;
 
 /**
@@ -21,15 +26,40 @@ public class OpenTunnelCommand extends AbstractHandler {
 
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		String containerName = event.getParameter(CONTAINER);
+		IZendTarget target = null;
 		
-		IZendTarget target = TargetsManagerService.INSTANCE.getContainerByName(containerName);
+		if (containerName != null) {
+			target = TargetsManagerService.INSTANCE
+					.getContainerByName(containerName);
+		} else {
+			EvaluationContext ctx = (EvaluationContext) event
+					.getApplicationContext();
+			Object element = ctx.getDefaultVariable();
+			if (element instanceof List) {
+				List<?> list = (List<?>) element;
+				if (list.size() > 0) {
+					element = list.get(0);
+				}
+			}
+			if (element instanceof IZendTarget) {
+				target = (IZendTarget) element;
+			}
+		}
 		if (target == null) {
 			throw new ExecutionException(NLS.bind("Unknown container '{0}'. Please add this container in Zend Studio to continue.", containerName)); 
 		}
 		
 		try {
-			ZendDevCloudTunnelManager.getManager().connect(target);
-		} catch (IOException e) {
+			if (!ZendDevCloudTunnelManager.getManager().connect(target)) {
+				throw new IllegalStateException(
+						"Openning SSH Tunnel is possible only for phpcloud targets.");
+			}
+		} catch (Exception e) {
+			Shell shell = PlatformUI.getWorkbench().getActiveWorkbenchWindow()
+					.getShell();
+			MessageDialog.openError(shell,
+					Messages.DeploymentHandler_sshTunnelErrorTitle,
+					e.getMessage());
 			throw new ExecutionException(NLS.bind("An error occured while connecting the SSH tunnel to '{0}' container: {1}", containerName, e.getMessage()), e);
 		}
 		
