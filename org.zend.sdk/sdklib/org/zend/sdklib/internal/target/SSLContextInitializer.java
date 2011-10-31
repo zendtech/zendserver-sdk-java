21f45a7287a11423a802cc71fe6cb2dce5dae864
ganoro@gmail.com <ganoro@gmail.com@b75f36dc-5d77-e969-e1c6-c1f8c8b91a4e>
2011-10-31 11:36:49 +0000

    ZSTD-1075 Project from Github cannot be created. Cannot retrieve branches.

--- org.zend.sdk/sdklib/org/zend/sdklib/internal/target/SSLContextInitializer.java
+++ org.zend.sdk/sdklib/org/zend/sdklib/internal/target/SSLContextInitializer.java
@@ -1,143 +1,93 @@
 package org.zend.sdklib.internal.target;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.security.KeyStore;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.NoSuchProviderException;
-import java.security.cert.CertificateException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
 
+import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.HttpsURLConnection;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.SSLSocketFactory;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.TrustManagerFactory;
-import javax.net.ssl.X509TrustManager;
 
 import org.restlet.Context;
 
 /**
- * Provides SSL initialization for default SSL factory and for restlet.
- * If global trust store System properties are set, then they're used. Otherwise,
- * ZendSdk own keystore is initialized and used in trust manager. 
+ * Provides SSL initialization for default SSL factory and for restlet. If
+ * global trust store System properties are set, then they're used. Otherwise,
+ * ZendSdk own keystore is initialized and used in trust manager.
  */
 public class SSLContextInitializer {
-	
-	private static final String ZENDSDK_KEYSTORE = System.getProperty("user.home") + File.separator + ".zendsdk.keystore";
+
+	private static final String ZENDSDK_KEYSTORE = System
+			.getProperty("user.home") + File.separator + ".zendsdk.keystore";
 	private static final String ZENDSDK_KEYSTORE_PASSWORD = "localstore";
 	private static final String ZENDSDK_KEYSTORE_RESOURCE = "localstore";
 
 	public static SSLContextInitializer instance = new SSLContextInitializer();
 
-	private SSLSocketFactory originalFactory;
-	private SSLSocketFactory localFactory;
-	
+	private static HostnameVerifier originalHostnameVerifier;
+
 	private SSLContextInitializer() {
 		// empty
 	}
-	
+
 	/**
 	 * Sets default SSL factory to use ZendSDK specific keystore
 	 */
 	public void setDefaultSSLFactory() {
-		if (localFactory == null) {
-			try {
-			localFactory = createLocalFactory();
-			} catch (Exception ex) {
-				ex.printStackTrace();
-			}
-		}
-		
-		originalFactory = HttpsURLConnection.getDefaultSSLSocketFactory();
-		if (originalFactory == localFactory) {
-			return;
-		}
-		
-		HttpsURLConnection.setDefaultSSLSocketFactory(localFactory);
+		originalHostnameVerifier = HttpsURLConnection
+				.getDefaultHostnameVerifier();
+		HttpsURLConnection
+				.setDefaultHostnameVerifier(new AllHostnameVerifier());
 	}
-	
+
 	/**
-	 * Restores Default SSL factory to the one used before calling {@link #setDefaultSSLFactory()}
+	 * Restores Default SSL factory to the one used before calling
+	 * {@link #setDefaultSSLFactory()}
 	 */
 	public void restoreDefaultSSLFactory() {
-		if (originalFactory != null) {
-			HttpsURLConnection.setDefaultSSLSocketFactory(originalFactory);
-		}
+		HttpsURLConnection.setDefaultHostnameVerifier(originalHostnameVerifier);
 	}
-	
+
 	/**
 	 * Retuns map with SSL trust manager settings.
 	 */
 	public Map<String, String> getSettings() {
 		Map<String, String> settings = new HashMap<String, String>();
-		settings.put("truststoreType", System.getProperty("javax.net.ssl.trustStoreType", "JKS"));
-		
+		settings.put("truststoreType",
+				System.getProperty("javax.net.ssl.trustStoreType", "JKS"));
+
 		String storePath = System.getProperty("javax.net.ssl.trustStore");
-		String storePassword = System.getProperty("javax.net.ssl.keyStorePassword");
+		String storePassword = System
+				.getProperty("javax.net.ssl.keyStorePassword");
 		if (storePath == null) {
 			storePassword = ZENDSDK_KEYSTORE_PASSWORD;
 			storePath = ZENDSDK_KEYSTORE;
 		}
-		
+
 		settings.put("truststorePassword", storePassword);
 		settings.put("truststorePath", storePath);
-		
+
 		return settings;
 	}
 
-	private SSLSocketFactory createLocalFactory() throws Exception {
+	private void createLocalFactory() throws Exception {
 		Map<String, String> settings = getSettings();
 		String storePath = settings.get("truststorePath");
-		String password = settings.get("truststorePassword");
-		String storeType = settings.get("truststoreType");
-		
-		if ((ZENDSDK_KEYSTORE.equals(storePath)) && (! new File(storePath).exists())) {
+
+		if ((ZENDSDK_KEYSTORE.equals(storePath))
+				&& (!new File(storePath).exists())) {
 			initializeZendKeyStore(ZENDSDK_KEYSTORE_RESOURCE, storePath);
 		}
-		
-		TrustManager[] myTMs = new TrustManager [] {
-                createTrustManager(storePath, password, storeType) };		
-		
-		SSLContext ctx = SSLContext.getInstance("TLS");
-		ctx.init(null, myTMs, null);
-		SSLContext.setDefault(ctx);
-		return ctx.getSocketFactory();
 	}
-	
-	private X509TrustManager createTrustManager(String keystore, String password, String storeType) throws KeyStoreException, NoSuchAlgorithmException, CertificateException, FileNotFoundException, IOException, NoSuchProviderException {
-		KeyStore ks = null;
-		if (keystore != null) {
-			ks = KeyStore.getInstance(storeType);
-			ks.load(new FileInputStream(keystore), password.toCharArray());
-		}
 
-		TrustManagerFactory tmf = TrustManagerFactory.getInstance("SunX509",
-				"SunJSSE");
-		tmf.init(ks);
-
-		TrustManager tms[] = tmf.getTrustManagers();
-
-		for (int i = 0; i < tms.length; i++) {
-			if (tms[i] instanceof X509TrustManager) {
-				return (X509TrustManager) tms[i];
-			}
-		}
-
-		throw new IllegalArgumentException("X509TrustManager not found.");
-	}
-	
-	private void initializeZendKeyStore(String resource, String keystore) throws IOException {
+	private void initializeZendKeyStore(String resource, String keystore)
+			throws IOException {
 		InputStream is = getClass().getResourceAsStream(resource);
 		FileOutputStream os = new FileOutputStream(keystore);
-		
+
 		byte[] buf = new byte[4096];
 		int len;
 		try {
@@ -149,17 +99,37 @@ public class SSLContextInitializer {
 			is.close();
 		}
 	}
-	
+
 	/**
 	 * Creates context with SSL security settings for restlet library.
 	 */
 	public Context getRestletContext() {
+		try {
+			createLocalFactory();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		Map<String, String> map = getSettings();
-		
+
 		Context ctx = new Context();
 		for (Entry<String, String> entry : map.entrySet()) {
 			ctx.getParameters().add(entry.getKey(), entry.getValue());
 		}
 		return ctx;
+	}
+
+	/**
+	 * This class implements a fake hostname verificator, trusting any host
+	 * name.
+	 */
+	public static class AllHostnameVerifier implements HostnameVerifier {
+
+		/**
+		 * Always return true, indicating that the host name is an acceptable
+		 * match with the server's authentication scheme.
+		 */
+		public boolean verify(String hostname, javax.net.ssl.SSLSession session) {
+			return (true);
+		}
 	}
 }
