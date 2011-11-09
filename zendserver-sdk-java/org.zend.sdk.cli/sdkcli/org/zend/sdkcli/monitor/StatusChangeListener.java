9555380e786376caa38ba203c6645c6e0dc07730
wgalanciak <wojciech.galanciak@gmail.com>
2011-11-09 18:10:56 +0000

    Add progress notification to deploy/update command

---
+++ zendserver-sdk-java/org.zend.sdk.cli/sdkcli/org/zend/sdkcli/monitor/StatusChangeListener.java
@@ -0,0 +1,47 @@
+package org.zend.sdkcli.monitor;
+
+import java.io.PrintWriter;
+
+import org.zend.webapi.core.progress.IStatus;
+import org.zend.webapi.core.progress.IStatusChangeEvent;
+import org.zend.webapi.core.progress.IStatusChangeListener;
+
+public class StatusChangeListener implements IStatusChangeListener {
+
+	private IStatus status;
+
+	private ProgressMonitor monitor;
+
+	public StatusChangeListener() {
+		monitor = new TextProgressMonitor(new PrintWriter(System.out));
+	}
+
+	public void statusChanged(IStatusChangeEvent event) {
+		status = event.getStatus();
+		switch (status.getCode()) {
+		case STARTING:
+			if (status.getTotalWork() == -1) {
+				monitor.beginTask(status.getMessage(), ProgressMonitor.UNKNOWN);
+			} else {
+				monitor.beginTask(status.getMessage(), status.getTotalWork());
+			}
+			break;
+		case STOPPING:
+			monitor.endTask();
+			break;
+		case PROCESSING:
+			monitor.update(status.getTotalWork());
+			break;
+		case ERROR:
+			monitor.endTask();
+			break;
+		default:
+			break;
+		}
+	}
+
+	public IStatus getStatus() {
+		return status;
+	}
+
+}
