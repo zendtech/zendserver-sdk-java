9b4b32a4893ee317cef55ab07ef84c656b226d60
wgalanciak <wojciech.galanciak@gmail.com>
2011-11-16 22:50:20 +0000

    Deploy application directly from git repository

--- org.zend.sdk.cli/sdkcli/org/zend/sdkcli/internal/commands/DeployApplicationCommand.java
+++ org.zend.sdk.cli/sdkcli/org/zend/sdkcli/internal/commands/DeployApplicationCommand.java
@@ -7,8 +7,11 @@
  *******************************************************************************/
 package org.zend.sdkcli.internal.commands;
 
+import java.io.File;
+import java.net.URISyntaxException;
 import java.text.MessageFormat;
 
+import org.eclipse.jgit.transport.URIish;
 import org.zend.sdkcli.internal.options.Option;
 import org.zend.sdkcli.monitor.StatusChangeListener;
 import org.zend.webapi.core.connection.data.ApplicationInfo;
@@ -21,17 +24,81 @@ import org.zend.webapi.core.connection.data.ApplicationInfo;
  */
 public class DeployApplicationCommand extends ApplicationAwareCommand {
 
+	private class DeployCloneCommand extends GitCloneProjectCommand {
+		@Override
+		public String getDir() {
+			try {
+				URIish uri = new URIish(DeployApplicationCommand.this.getRepo());
+				return uri.getHumanishName();
+			} catch (URISyntaxException e) {
+				getLogger().error(e);
+			}
+			return null;
+		}
+
+		@Override
+		public String getRepo() {
+			return DeployApplicationCommand.this.getRepo();
+		}
+
+		@Override
+		public String getUser() {
+			return DeployApplicationCommand.this.getUser();
+		}
+
+		@Override
+		public String getPassword() {
+			return DeployApplicationCommand.this.getPassword();
+		}
+		
+		@Override
+		public String getKey() {
+			return DeployApplicationCommand.this.getKey();
+		}
+
+		@Override
+		public String getBranch() {
+			return DeployApplicationCommand.this.getBranch();
+		}
+
+		@Override
+		public boolean isVerbose() {
+			return DeployApplicationCommand.this.isVerbose();
+		}
+		
+		public File getProjectFile() {
+			try {
+				if (getRepo() != null) {
+					return getDirectory(getRepo());
+				}
+			} catch (URISyntaxException e) {
+				getLogger().error(e);
+			}
+			return null;
+		}
+	}
+
 	private static final String PATH = "p";
 	private static final String BASE_PATH = "b";
 	private static final String PARAMS = "m";
 	private static final String NAME = "n";
 	private static final String IGNORE_FAILURES = "f";
 	private static final String VHOST = "h";
+	private static final String REPO = "r";
+	private static final String USER = "u";
+	private static final String PASSWD = "p";
+	private static final String KEY = "k";
+	private static final String BRANCH = "g";
+
+	private DeployCloneCommand cloneCommand = new DeployCloneCommand();
 
 	@Option(opt = PATH, required = false, description = "Application package location or project's directory, if not provided current directory is considered", argName = "path")
 	public String getPath() {
 		final String value = getValue(PATH);
 		if (value == null) {
+			if (getRepo() != null && cloneCommand.getProjectFile() != null) {
+				return cloneCommand.getProjectFile().getAbsolutePath();
+			}
 			return getCurrentDirectory();
 		}
 		return value;
@@ -62,23 +129,80 @@ public class DeployApplicationCommand extends ApplicationAwareCommand {
 		return getValue(VHOST);
 	}
 
+	@Option(opt = REPO, required = false, description = "Repository to clone from", argName = "repository")
+	public String getRepo() {
+		final String value = getValue(REPO);
+		return value;
+	}
+
+	@Option(opt = USER, required = false, description = "User name for git repository access", argName = "user")
+	public String getUser() {
+		final String value = getValue(USER);
+		return value;
+	}
+
+	@Option(opt = PASSWD, required = false, description = "Password for git repository access", argName = "password")
+	public String getPassword() {
+		final String value = getValue(PASSWD);
+		return value;
+	}
+
+	@Option(opt = KEY, required = false, description = "Path to SSH private key for git repository access", argName = "key")
+	public String getKey() {
+		final String value = getValue(KEY);
+		return value;
+	}
+
+	@Option(opt = BRANCH, required = false, description = "Initial branch to check out project from git repository", argName = "branch")
+	public String getBranch() {
+		final String value = getValue(BRANCH);
+		return value;
+	}
+
 	public boolean isVhost() {
 		return getVhost() != null;
 	}
 
 	@Override
 	public boolean doExecute() {
-		getApplication().addStatusChangeListener(new StatusChangeListener());
-		ApplicationInfo info = getApplication().deploy(getPath(),
-				getBasePath(), getTargetId(), getParams(), getName(),
-				isIgnoreFailures(), getVhost(), !isVhost());
-		if (info == null) {
-			return false;
+		boolean result = true;
+		if (getRepo() != null) {
+			result = cloneCommand.doExecute();
 		}
-		getLogger().info(
-				MessageFormat.format(
-						"Application {0} (id {1}) is deployed to {2}",
-						info.getAppName(), info.getId(), info.getBaseUrl()));
-		return true;
+		if (result) {
+			try {
+				getApplication()
+						.addStatusChangeListener(new StatusChangeListener());
+				ApplicationInfo info = getApplication().deploy(getPath(),
+						getBasePath(), getTargetId(), getParams(), getName(),
+						isIgnoreFailures(), getVhost(), !isVhost());
+				if (info != null) {
+					getLogger()
+							.info(MessageFormat
+									.format("Application {0} (id {1}) is deployed to {2}",
+											info.getAppName(), info.getId(),
+											info.getBaseUrl()));
+					return true;
+				}
+			} finally {
+				if (getRepo() != null && cloneCommand.getProjectFile() != null) {
+					delete(cloneCommand.getProjectFile());
+				}
+			}
+		}
+		return false;
+	}
+
+	private boolean delete(File file) {
+		if (file.isDirectory()) {
+			String[] children = file.list();
+			for (int i = 0; i < children.length; i++) {
+				boolean result = delete(new File(file, children[i]));
+				if (!result) {
+					return false;
+				}
+			}
+		}
+		return file.delete();
 	}
 }
