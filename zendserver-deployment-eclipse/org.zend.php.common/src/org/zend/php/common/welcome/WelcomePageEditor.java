44f99b4eeb8c6756e51ec3aa7a55d359ca3742b7
Jacek Pospychala <jacek.pospychala@gmail.com>
2012-06-11 09:03:05 +0000

    ZSTD-1622 After upgrade PDT->Studio, PDT welcome page should be closed and Studio welcome should show up

--- zendserver-deployment-eclipse/org.zend.php.common/src/org/zend/php/common/welcome/WelcomePageEditor.java
+++ zendserver-deployment-eclipse/org.zend.php.common/src/org/zend/php/common/welcome/WelcomePageEditor.java
@@ -48,7 +48,6 @@ public class WelcomePageEditor extends WebBrowserEditor {
 	private ZendCatalogViewer viewer;
 
 	public WelcomePageEditor() {
-		PdtStats.visit("http://www.zend.com/en/community/pdt/upgrade");
 		// TODO Auto-generated constructor stub
 	}
 
