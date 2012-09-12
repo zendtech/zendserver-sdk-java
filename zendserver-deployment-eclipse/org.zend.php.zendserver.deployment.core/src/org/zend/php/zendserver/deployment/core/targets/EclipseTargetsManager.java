6dd8c2e9b0c2c0ca2cdb5d1a8dfe83d6570e9103
Wojciech Galanciak <wojciech.galanciak@gmail.com>
2012-09-12 14:36:07 +0000

    ZSTD-2128 [OpenShift] Provide ability to customize OpenShift server

--- zendserver-deployment-eclipse/org.zend.php.zendserver.deployment.core/src/org/zend/php/zendserver/deployment/core/targets/EclipseTargetsManager.java
+++ zendserver-deployment-eclipse/org.zend.php.zendserver.deployment.core/src/org/zend/php/zendserver/deployment/core/targets/EclipseTargetsManager.java
@@ -1,30 +1,38 @@
 package org.zend.php.zendserver.deployment.core.targets;
 
+import org.eclipse.core.runtime.preferences.IEclipsePreferences;
+import org.eclipse.core.runtime.preferences.InstanceScope;
+import org.zend.php.zendserver.deployment.core.DeploymentCore;
 import org.zend.sdklib.SdkException;
+import org.zend.sdklib.internal.target.OpenShiftTarget;
 import org.zend.sdklib.internal.target.ZendDevCloud;
 import org.zend.sdklib.manager.TargetException;
 import org.zend.sdklib.manager.TargetsManager;
 import org.zend.sdklib.target.IZendTarget;
 
 public class EclipseTargetsManager extends TargetsManager {
-	
+
 	public EclipseTargetsManager() {
 		super();
-		
+		IEclipsePreferences prefs = InstanceScope.INSTANCE
+				.getNode(DeploymentCore.PLUGIN_ID);
+		OpenShiftTarget.iniLibraServer(prefs.get(
+				OpenShiftTarget.LIBRA_SERVER_PROP,
+				OpenShiftTarget.getDefaultLibraServer()));
 		// TODO on startup check if there are targets added from command-line
-		
+
 	}
-	
+
 	@Override
 	public synchronized IZendTarget add(IZendTarget target,
 			boolean suppressConnect) throws TargetException {
-		
+
 		IZendTarget result = super.add(target, suppressConnect);
-		
+
 		if (result != null) {
 			EclipseSSH2Settings.registerDevCloudTarget(result, false);
 		}
-		
+
 		ZendDevCloud cloud = new ZendDevCloud();
 		if (cloud.isCloudTarget(target)) {
 			cloud.setPublicKeyBuilder(new JSCHPubKeyDecryptor());
@@ -34,7 +42,7 @@ public class EclipseTargetsManager extends TargetsManager {
 				throw new TargetException(e);
 			}
 		}
-		
+
 		return result;
 	}
 
@@ -43,11 +51,12 @@ public class EclipseTargetsManager extends TargetsManager {
 		return super.remove(target);
 		// TODO remove SSH keys for removed targets
 	}
-	
+
 	@Override
 	public IZendTarget updateTarget(String targetId, String host,
 			String defaultServer, String key, String secretKey) {
-		return super.updateTarget(targetId, host, defaultServer, key, secretKey);
+		return super
+				.updateTarget(targetId, host, defaultServer, key, secretKey);
 		// TODO update SSH key?
 	}
 }
