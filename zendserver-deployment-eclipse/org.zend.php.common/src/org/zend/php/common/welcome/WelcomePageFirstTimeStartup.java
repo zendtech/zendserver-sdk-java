ac678103bccbc11eb338592678d79ebae8892906
Natalia Bartol <natalia.bartol@gmail.com>
2012-09-27 14:16:18 +0000

    Removing unused variable

--- zendserver-deployment-eclipse/org.zend.php.common/src/org/zend/php/common/welcome/WelcomePageFirstTimeStartup.java
+++ zendserver-deployment-eclipse/org.zend.php.common/src/org/zend/php/common/welcome/WelcomePageFirstTimeStartup.java
@@ -4,12 +4,10 @@ import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.commands.NotEnabledException;
 import org.eclipse.core.commands.NotHandledException;
 import org.eclipse.core.commands.common.NotDefinedException;
-import org.eclipse.core.runtime.preferences.ConfigurationScope;
 import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.ui.IStartup;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.handlers.IHandlerService;
-import org.osgi.service.prefs.Preferences;
 import org.zend.php.common.Activator;
 
 public class WelcomePageFirstTimeStartup implements IStartup {
@@ -20,10 +18,6 @@ public class WelcomePageFirstTimeStartup implements IStartup {
 	public static void run() {
 		// Workspace specific preferences
 		IPreferenceStore pref = Activator.getDefault().getPreferenceStore();
-
-		// Configuration specific preferences
-		Preferences preferences = ConfigurationScope.INSTANCE
-				.getNode(Activator.PLUGIN_ID);
 
 		if (!pref.getBoolean(WAS_FIRST_STARTUP)
 				|| pref.getBoolean(SHOW_WELCOME)) {
