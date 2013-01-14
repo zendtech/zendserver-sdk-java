6e7b184788cacc6f6f8c9b69f2401bd39d05004e
Wojciech Galanciak <wojciech.galanciak@gmail.com>
2013-01-14 23:02:32 +0000

    ZSTD-2634 deployment.xml -> Triggers -> deployment script issues

--- zendserver-deployment-eclipse/org.zend.php.zendserver.deployment.ui/src/org/zend/php/zendserver/deployment/ui/editors/ScriptsTreeSection.java
+++ zendserver-deployment-eclipse/org.zend.php.zendserver.deployment.ui/src/org/zend/php/zendserver/deployment/ui/editors/ScriptsTreeSection.java
@@ -1,10 +1,17 @@
 package org.zend.php.zendserver.deployment.ui.editors;
 
+import java.util.List;
+
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.forms.editor.FormEditor;
 import org.eclipse.ui.forms.widgets.FormToolkit;
+import org.zend.php.zendserver.deployment.core.descriptor.IDeploymentDescriptor;
 import org.zend.php.zendserver.deployment.core.descriptor.IDescriptorContainer;
 import org.zend.php.zendserver.deployment.ui.Messages;
+import org.zend.sdklib.mapping.IMapping;
+import org.zend.sdklib.mapping.IMappingChangeEvent;
+import org.zend.sdklib.mapping.IMappingEntry;
+import org.zend.sdklib.mapping.IMappingEntry.Type;
 import org.zend.sdklib.mapping.IMappingModel;
 
 public class ScriptsTreeSection extends PropertiesTreeSection {
@@ -20,5 +27,24 @@ public class ScriptsTreeSection extends PropertiesTreeSection {
 	protected String getFolder() {
 		return IMappingModel.SCRIPTSDIR;
 	}
+	
+	@Override
+	public void mappingChanged(IMappingChangeEvent event) {
+		super.mappingChanged(event);
+		IMappingModel mappingModel = model.getMappingModel();
+		IMappingEntry entry = mappingModel.getEntry(IMappingModel.SCRIPTSDIR, Type.INCLUDE);
+		if (entry != null) {
+			List<IMapping> mappings = entry.getMappings();
+			if (mappings != null && mappings.size() > 0) {
+				IDeploymentDescriptor descriptor = model.getDescriptorModel();
+				if (descriptor.getScriptsRoot() == null
+						|| descriptor.getScriptsRoot().isEmpty()) {
+					descriptor.setScriptsRoot("scripts"); //$NON-NLS-1$
+				}
+			}
+		}
+	}
+	
+	
 
 }
