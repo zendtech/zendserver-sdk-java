3cfe88bac204b26dfe3dcef6ad871b60600634e3
wgalanciak <wojciech.galanciak@gmail.com>
2013-03-08 15:05:11 +0000

    ZSTD-2863 [Forums] Some events do not pop-up in Studio after performing deletion of events on the server ZSTD-2872 [Application Monitoring] Improve monitoring initialization for Zend Server 6

--- zendserver-sdk-java/org.zend.sdk/sdklib/org/zend/sdklib/monitor/ZendMonitor.java
+++ zendserver-sdk-java/org.zend.sdk/sdklib/org/zend/sdklib/monitor/ZendMonitor.java
@@ -9,17 +9,22 @@ package org.zend.sdklib.monitor;
 
 import java.net.MalformedURLException;
 import java.text.MessageFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import org.zend.sdklib.internal.application.ZendConnection;
 import org.zend.sdklib.internal.monitor.ZendIssue;
 import org.zend.sdklib.mapping.IMappingLoader;
 import org.zend.sdklib.target.ITargetLoader;
+import org.zend.sdklib.target.IZendTarget;
 import org.zend.webapi.core.WebApiClient;
 import org.zend.webapi.core.WebApiException;
 import org.zend.webapi.core.connection.data.Issue;
 import org.zend.webapi.core.connection.data.IssueList;
+import org.zend.webapi.core.connection.data.values.ZendServerVersion;
 import org.zend.webapi.core.progress.BasicStatus;
 import org.zend.webapi.core.progress.StatusCode;
 
@@ -199,6 +204,56 @@ public class ZendMonitor extends ZendConnection {
 		return null;
 	}
 
+	public List<IZendIssue> getIssues(Filter filter, long lastTime,
+			IZendTarget target) {
+		List<IZendIssue> result = new ArrayList<IZendIssue>();
+		int offset = 0;
+		try {
+			WebApiClient client = getClient();
+			while (true) {
+				IssueList list = client.monitorGetIssuesListPredefinedFilter(
+						filter.getName(), 20, offset, "id", "DESC");
+				if (list != null) {
+					List<IZendIssue> issues = ZendIssue.create(
+							list.getIssues(), this);
+					if (issues != null && issues.size() > 0) {
+						for (IZendIssue issue : issues) {
+							Date lastDate = getTime(issue.getIssue()
+									.getLastOccurance(), target);
+							if (lastDate != null) {
+								long time = lastDate.getTime();
+								if (time > lastTime) {
+									result.add(issue);
+								} else {
+									return result;
+								}
+							}
+						}
+						offset += 20;
+					} else {
+						break;
+					}
+				} else {
+					break;
+				}
+			}
+		} catch (WebApiException e) {
+			String message = MessageFormat.format(
+					"Error during retrieving open issues from '{0}'", targetId);
+			notifier.statusChanged(new BasicStatus(StatusCode.ERROR,
+					"Retrieving Open Issues", message, e));
+			log.error(message + "':");
+			log.error("\tpossible error: " + e.getMessage());
+		} catch (MalformedURLException e) {
+			String message = MessageFormat.format(
+					"Error during retrieving open issues from '{0}'", targetId);
+			notifier.statusChanged(new BasicStatus(StatusCode.ERROR,
+					"Retrieving Open Issues", message, e));
+			log.error(e);
+		}
+		return result;
+	}
+
 	/**
 	 * Provides list of issues by using {@link Filter#CRITICAL_ERRORS} filter.
 	 * 
@@ -260,8 +315,46 @@ public class ZendMonitor extends ZendConnection {
 		return null;
 	}
 
+	public long getLastEventTime(List<IZendIssue> issues, IZendTarget target) {
+		if (issues != null && issues.size() > 0) {
+			Date lastDate = getTime(issues.get(issues.size() - 1).getIssue()
+					.getLastOccurance(), target);
+			if (lastDate != null) {
+				return lastDate.getTime();
+			}
+		}
+		return -1;
+	}
+
 	public WebApiClient getClient() throws MalformedURLException {
 		return getClient(targetId);
+	}
+
+	public Date getTime(String time, IZendTarget target) {
+		SimpleDateFormat formatter = null;
+		if (ZendServerVersion
+				.byName(target.getProperty(IZendTarget.SERVER_VERSION))
+				.getName().startsWith("6")) { //$NON-NLS-1$
+			int index = time.indexOf("T"); //$NON-NLS-1$
+			String prefix = time.substring(0, index);
+			String suffix = time.substring(index + 1, time.length());
+			time = prefix + ' ' + suffix;
+			index = time.lastIndexOf('+');
+			if (index == -1) {
+				index = time.lastIndexOf('-');
+			}
+			time = time.substring(0, index);
+			formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"); //$NON-NLS-1$
+		} else {
+			formatter = new SimpleDateFormat("dd-MMM-yyyy HH:mm"); //$NON-NLS-1$
+		}
+		Date date = null;
+		try {
+			date = formatter.parse(time);
+		} catch (ParseException e) {
+			return null;
+		}
+		return date;
 	}
 
 	private List<Issue> doGetIssues(Filter filter)
