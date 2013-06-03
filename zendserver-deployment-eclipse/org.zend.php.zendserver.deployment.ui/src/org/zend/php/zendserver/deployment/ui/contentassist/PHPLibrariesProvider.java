a7b8ce241291fcd9bfea047dbf83b5f0af6bf7d7
Wojciech Galanciak <wojciech.galanciak@gmail.com>
2013-06-03 14:03:15 +0000

    ZSTD-3121 [Libraries] Content Assist suggests wrong items in PHP Library Dependencies dialog.

--- zendserver-deployment-eclipse/org.zend.php.zendserver.deployment.ui/src/org/zend/php/zendserver/deployment/ui/contentassist/PHPLibrariesProvider.java
+++ zendserver-deployment-eclipse/org.zend.php.zendserver.deployment.ui/src/org/zend/php/zendserver/deployment/ui/contentassist/PHPLibrariesProvider.java
@@ -2,11 +2,16 @@ package org.zend.php.zendserver.deployment.ui.contentassist;
 
 import java.util.Arrays;
 
+import org.eclipse.dltk.core.DLTKCore;
+import org.eclipse.php.internal.core.PHPLanguageToolkit;
+
 public class PHPLibrariesProvider implements IProposalProvider {
 
 	private static String[] libraries;
 
 	public void init() {
+		libraries = DLTKCore.getUserLibraryNames(PHPLanguageToolkit
+				.getDefault());
 		Arrays.sort(libraries, String.CASE_INSENSITIVE_ORDER);
 	}
 
