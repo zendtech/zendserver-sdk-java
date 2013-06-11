bf0ec27ec6819b00cd954267dccc26a51ad055d0
Wojciech Galanciak <wojciech.galanciak@gmail.com>
2013-06-11 21:36:54 +0000

    ZSTD-3167 [Linux] Triggers tab -> Parameter details section - wrong alignment for the value in Description field

--- zendserver-deployment-eclipse/org.zend.php.zendserver.deployment.ui/src/org/zend/php/zendserver/deployment/ui/editors/ParameterDetailsPage.java
+++ zendserver-deployment-eclipse/org.zend.php.zendserver.deployment.ui/src/org/zend/php/zendserver/deployment/ui/editors/ParameterDetailsPage.java
@@ -146,9 +146,7 @@ public class ParameterDetailsPage extends DescriptorDetailsPage {
 			}
 		});
 		
-		EditorField description = fields.add(new TextField(null,
-				DeploymentDescriptorPackage.PARAM_DESCRIPTION,
-				Messages.ParameterDetailsPage_Description, SWT.MULTI, false));
+		EditorField description = fields.add(new TextField(null, DeploymentDescriptorPackage.PARAM_DESCRIPTION, Messages.ParameterDetailsPage_Description));
 		description.create(client, toolkit);
 		GridData gd = ((GridData)description.getText().getLayoutData());
 		gd.heightHint = 100;
