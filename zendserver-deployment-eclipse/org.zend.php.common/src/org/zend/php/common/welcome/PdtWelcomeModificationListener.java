f05293255ee31ffcd4fc714cfded99ec737b2bac
Natalia Bartol <natalia.bartol>
2013-06-17 13:50:44 +0000

    Installation of extra features unrelated to Studio should not force Studio installation.

--- zendserver-deployment-eclipse/org.zend.php.common/src/org/zend/php/common/welcome/PdtWelcomeModificationListener.java
+++ zendserver-deployment-eclipse/org.zend.php.common/src/org/zend/php/common/welcome/PdtWelcomeModificationListener.java
@@ -22,71 +22,72 @@ public class PdtWelcomeModificationListener implements
 	private static final String STUDIO_IU = "com.zend.php.ide";
 	private static final String PDT_PRODUCT_ID = "org.zend.php.product";
 	private IStatus status;
-	
+
 	public IStatus aboutToChange(final Collection<String> setToAdd,
 			final Collection<String> setToRemove) {
-		if (! isPDtProduct()) { // do nothing, if we're not in PDT product
-			return Status.OK_STATUS;
+		status = Status.OK_STATUS;
+
+		if (!isPDtProduct()) { // do nothing, if we're not in PDT product
+			return status;
 		}
-		
+
 		RevertUtil ru = new RevertUtil();
 		ru.setRevertTimestamp();
-		status = null;
-		
+
 		Display.getDefault().syncExec(new Runnable() {
-			
+
 			public void run() {
 				Shell parent = Display.getDefault().getActiveShell();
-				boolean ok = MessageDialog.openConfirm(parent, "About to Upgrade", "You are about to upgrade to Zend Studio 30 day free trial. Press OK to Continue, or Cancel.");
-				if (ok) {
-					if (! setToAdd.contains(STUDIO_IU)) {
-						setToAdd.add(STUDIO_IU);
+				if (setToAdd.contains(STUDIO_IU)) {
+					boolean ok = MessageDialog
+							.openConfirm(
+									parent,
+									"About to Upgrade",
+									"You are about to upgrade to Zend Studio 30 day free trial. Press OK to Continue, or Cancel.");
+					if (!ok) {
+						status = Status.CANCEL_STATUS;
 					}
-					
-					if (setToRemove.contains(STUDIO_IU)) {
-						setToRemove.remove(STUDIO_IU);
-					}
-				} else {
-					status = Status.CANCEL_STATUS;
 				}
 			}
 		});
-		
+
 		return status;
 	}
 
 	public void profileChanged(Collection<String> setToAdd,
 			Collection<String> setToRemove, IStatus status) {
-		if (! isPDtProduct()) {  // do nothing, if we're not in PDT product
+		if (!isPDtProduct()) { // do nothing, if we're not in PDT product
 			return;
 		}
-		
-		if ((setToAdd != null) && (setToAdd.contains(STUDIO_IU)) && status.getSeverity() == IStatus.OK) {
+
+		if ((setToAdd != null) && (setToAdd.contains(STUDIO_IU))
+				&& status.getSeverity() == IStatus.OK) {
 			RevertUtil ru = new RevertUtil();
 			ru.setRevertTimestamp();
-			
+
 			// enforce welcome page on upgraded product start
 			WelcomePageFirstTimeStartup.disableFirstStartup(false);
 			closeWelcomeEditor();
-			
+
 			PdtStats.visit("http://updates.zend.com/studio/pdt/?upgrade");
 		}
 	}
 
 	public static void closeWelcomeEditor() {
 		Display.getDefault().asyncExec(new Runnable() {
-			
+
 			public void run() {
-				IWorkbenchWindow window = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+				IWorkbenchWindow window = PlatformUI.getWorkbench()
+						.getActiveWorkbenchWindow();
 				if (window == null) {
 					return;
 				}
-				
+
 				IWorkbenchPage page = window.getActivePage();
 				if (page == null) {
 					return;
 				}
-				
+
 				IEditorReference[] editorRefs = page.getEditorReferences();
 				for (IEditorReference editorRef : editorRefs) {
 					if (WelcomePageEditor.EDITOR_ID.equals(editorRef.getId())) {
