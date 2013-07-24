407dae0e84c48a73649d727ad40a854958aa2a36
wgalanciak <wojciech.galanciak@gmail.com>
2013-07-24 19:36:12 +0000

    ZSTD-3318 [Zend SDK] ZS6 targets show status disconnected

--- org.zend.sdk.cli/sdkcli/org/zend/sdkcli/internal/commands/ListTargetsCommand.java
+++ org.zend.sdk.cli/sdkcli/org/zend/sdkcli/internal/commands/ListTargetsCommand.java
@@ -13,6 +13,11 @@ import org.zend.sdkcli.internal.options.Option;
 import org.zend.sdklib.target.IZendTarget;
 import org.zend.sdklib.target.LicenseExpiredException;
 import org.zend.webapi.core.WebApiException;
+import org.zend.webapi.core.connection.data.values.ServerType;
+import org.zend.webapi.core.connection.data.values.WebApiVersion;
+import org.zend.webapi.core.connection.response.ResponseCode;
+import org.zend.webapi.internal.core.connection.exception.UnexpectedResponseCode;
+import org.zend.webapi.internal.core.connection.exception.WebApiCommunicationError;
 
 public class ListTargetsCommand extends TargetAwareCommand {
 
@@ -40,11 +45,10 @@ public class ListTargetsCommand extends TargetAwareCommand {
 			commandLine.getLog().info("\tKey: " + target.getKey());
 
 			if (isStatus()) {
-				boolean connect = false;
+				IZendTarget t = null;
 				try {
-					connect = target.connect();
+				t = testTargetConnection(target);
 				} catch (WebApiException e) {
-					connect = false;
 				} catch (LicenseExpiredException e) {
 					getLogger()
 							.error(MessageFormat
@@ -53,11 +57,42 @@ public class ListTargetsCommand extends TargetAwareCommand {
 				}
 				commandLine.getLog()
 						.error("\tStatus: "
-								+ (connect ? "connected" : "disconnected"));
+								+ (t != null ? "connected" : "disconnected"));
 			}
 
 		}
 
 		return true;
+	}
+	
+	public IZendTarget testTargetConnection(IZendTarget target)
+			throws WebApiException, LicenseExpiredException {
+		try {
+			if (target.connect(WebApiVersion.V1_3, ServerType.ZEND_SERVER)) {
+				return target;
+			}
+		} catch (WebApiCommunicationError e) {
+			throw e;
+		} catch (UnexpectedResponseCode e) {
+			ResponseCode code = e.getResponseCode();
+			switch (code) {
+			case INTERNAL_SERVER_ERROR:
+			case AUTH_ERROR:
+			case INSUFFICIENT_ACCESS_LEVEL:
+				throw e;
+			default:
+				break;
+			}
+		}
+		try {
+			if (target.connect(WebApiVersion.UNKNOWN, ServerType.ZEND_SERVER)) {
+				return target;
+			}
+		} catch (WebApiException ex) {
+			if (target.connect()) {
+				return target;
+			}
+		}
+		return null;
 	}
 }
