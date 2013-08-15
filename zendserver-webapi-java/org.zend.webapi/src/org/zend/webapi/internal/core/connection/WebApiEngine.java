74056700ab90e0a1545163b2ea2096f0f28590ac
wgalanciak <wojciech.galanciak@gmail.com>
2013-08-15 15:16:56 +0200

    ZSTD-3367 [Zend SDK] [Regression] Cannot add cloud targets - add support for https

--- zendserver-webapi-java/org.zend.webapi/src/org/zend/webapi/internal/core/connection/WebApiEngine.java
+++ zendserver-webapi-java/org.zend.webapi/src/org/zend/webapi/internal/core/connection/WebApiEngine.java
@@ -4,10 +4,12 @@ import org.restlet.engine.Engine;
 import org.restlet.engine.connector.HttpServerHelper;
 import org.restlet.engine.local.RiapClientHelper;
 import org.restlet.engine.local.RiapServerHelper;
+import org.restlet.ext.httpclient.HttpClientHelper;
 
 public class WebApiEngine extends Engine {
 
 	public void registerDefaultConnectors() {
+		getRegisteredClients().add(new HttpClientHelper(null));
 		getRegisteredClients().add(new WebApiHttpClientHelper(null));
 		getRegisteredClients().add(
 				new org.restlet.engine.local.ClapClientHelper(null));
