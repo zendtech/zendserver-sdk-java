72f30057d1146387f2daa6a1e971afa20f2ce023
wgalanciak <wojciech.galanciak@gmail.com>
2013-08-16 15:51:55 +0200

    Add proper message for redeploy command on success

--- org.zend.sdk.cli/sdkcli/org/zend/sdkcli/internal/commands/RedeployApplicationCommand.java
+++ org.zend.sdk.cli/sdkcli/org/zend/sdkcli/internal/commands/RedeployApplicationCommand.java
@@ -7,6 +7,8 @@
  *******************************************************************************/
 package org.zend.sdkcli.internal.commands;
 
+import java.text.MessageFormat;
+
 import org.zend.sdkcli.internal.options.Option;
 import org.zend.webapi.core.connection.data.ApplicationInfo;
 
@@ -21,8 +23,8 @@ public class RedeployApplicationCommand extends ApplicationAwareCommand {
 	private static final String APPID = "a";
 	private static final String IGNORE_FAILURES = "i";
 	private static final String SERVERS = "s";
-	
-	@Option(opt = APPID, required = true, description = "Application id to redeploy", argName="app-id")
+
+	@Option(opt = APPID, required = true, description = "Application id to redeploy", argName = "app-id")
 	public String getApplicationId() {
 		return getValue(APPID);
 	}
@@ -32,20 +34,24 @@ public class RedeployApplicationCommand extends ApplicationAwareCommand {
 		return hasOption(IGNORE_FAILURES);
 	}
 
-	@Option(opt = SERVERS, required = false, description = "Server names", argName="server-names")
+	@Option(opt = SERVERS, required = false, description = "Server names", argName = "server-names")
 	public String[] getServers() {
 		return getValues(SERVERS);
 	}
 
 	@Override
 	public boolean doExecute() {
-		ApplicationInfo info = getApplication().redeploy(getTargetId(), getApplicationId(),
-			getServers(), isIgnoreFailures());
-		
-		if (info == null) {
-			return false;
+		ApplicationInfo info = getApplication().redeploy(getTargetId(),
+				getApplicationId(), getServers(), isIgnoreFailures());
+
+		if (info != null) {
+			getLogger()
+					.info(MessageFormat
+							.format("Application {0} (id {1}) was redeployed successfully on target {2}",
+									info.getAppName(), info.getId(), getTargetId()));
+			return true;
 		}
-		return true;
+		return false;
 	}
 
 }
