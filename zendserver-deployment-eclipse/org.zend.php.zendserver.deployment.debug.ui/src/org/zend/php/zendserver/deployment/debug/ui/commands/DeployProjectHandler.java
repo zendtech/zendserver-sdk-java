03cf1617984307bdf73a3d90adf69709223ea606
wgalanciak <wojciech.galanciak@gmail.com>
2014-06-09 13:18:21 +0200

    ZSTD-4229 Add deployment from project context menu

--- zendserver-deployment-eclipse/org.zend.php.zendserver.deployment.debug.ui/src/org/zend/php/zendserver/deployment/debug/ui/commands/DeployProjectHandler.java
+++ zendserver-deployment-eclipse/org.zend.php.zendserver.deployment.debug.ui/src/org/zend/php/zendserver/deployment/debug/ui/commands/DeployProjectHandler.java
@@ -13,10 +13,13 @@ package org.zend.php.zendserver.deployment.debug.ui.commands;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IProjectDescription;
 import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.jface.viewers.ISelection;
@@ -24,8 +27,10 @@ import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.handlers.HandlerUtil;
 import org.zend.php.server.core.utils.ServerUtils;
+import org.zend.php.zendserver.deployment.core.DeploymentNature;
 import org.zend.php.zendserver.deployment.core.debugger.IDeploymentHelper;
 import org.zend.php.zendserver.deployment.debug.core.config.LaunchUtils;
+import org.zend.php.zendserver.deployment.debug.ui.Activator;
 import org.zend.php.zendserver.deployment.debug.ui.config.DeploymentHandler;
 import org.zend.sdklib.target.IZendTarget;
 
@@ -70,25 +75,54 @@ public class DeployProjectHandler extends AbstractDeploymentHandler {
 
 			@Override
 			protected IStatus run(IProgressMonitor monitor) {
-				DeploymentHandler handler = new DeploymentHandler();
-				IDeploymentHelper defaultHelper = null;
-
-				if (targetId != null) {
-					defaultHelper = LaunchUtils.createDefaultHelper(targetId,
-							project);
-				} else {
-					defaultHelper = LaunchUtils.createDefaultHelper(project);
+				try {
+					if (!hasDeploymentNature(project)) {
+						enableDeployment(project);
+					}
+					DeploymentHandler handler = new DeploymentHandler();
+					IDeploymentHelper defaultHelper = null;
+					if (targetId != null) {
+						defaultHelper = LaunchUtils.createDefaultHelper(
+								targetId, project);
+					} else {
+						defaultHelper = LaunchUtils
+								.createDefaultHelper(project);
+					}
+					if (handler.openNoConfigDeploymentWizard(defaultHelper,
+							project) != IStatus.OK) {
+						return Status.CANCEL_STATUS;
+					}
+				} catch (CoreException e) {
+					Activator.log(e);
+					return new Status(IStatus.ERROR, Activator.PLUGIN_ID,
+							e.getMessage(), e);
 				}
-				if (handler
-						.openNoConfigDeploymentWizard(defaultHelper, project) == IStatus.OK) {
-					return Status.OK_STATUS;
-				} else {
-					return Status.CANCEL_STATUS;
-				}
+				return Status.OK_STATUS;
 			}
 		};
 		job.setSystem(true);
 		job.schedule();
+	}
+
+	protected boolean hasDeploymentNature(IProject project)
+			throws CoreException {
+		String[] natures = project.getDescription().getNatureIds();
+		for (String nature : natures) {
+			if (DeploymentNature.ID.equals(nature)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	protected void enableDeployment(IProject project) throws CoreException {
+		IProjectDescription desc = project.getDescription();
+		String[] natures = desc.getNatureIds();
+		String[] nnatures = new String[natures.length + 1];
+		System.arraycopy(natures, 0, nnatures, 0, natures.length);
+		nnatures[natures.length] = DeploymentNature.ID;
+		desc.setNatureIds(nnatures);
+		project.setDescription(desc, new NullProgressMonitor());
 	}
 
 }
--- zendserver-deployment-eclipse/org.zend.php.zendserver.deployment.ui/src/org/zend/php/zendserver/deployment/ui/commands/DeploymentTester.java
+++ zendserver-deployment-eclipse/org.zend.php.zendserver.deployment.ui/src/org/zend/php/zendserver/deployment/ui/commands/DeploymentTester.java
@@ -11,13 +11,10 @@
 package org.zend.php.zendserver.deployment.ui.commands;
 
 import org.eclipse.core.expressions.PropertyTester;
-import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IAdaptable;
-import org.zend.php.server.core.utils.ServerUtils;
-import org.zend.php.zendserver.deployment.core.descriptor.DescriptorContainerManager;
 
 /**
  * Property tester for deployment related commands.
@@ -27,13 +24,11 @@ import org.zend.php.zendserver.deployment.core.descriptor.DescriptorContainerMan
  */
 public class DeploymentTester extends PropertyTester {
 
-	private static final String QUICK_DEPLOY = "quickDeploy"; //$NON-NLS-1$
-	private static final String DEPLOY = "deploy"; //$NON-NLS-1$
-	private static final String ADD_SUPPORT = "addSupport"; //$NON-NLS-1$
 	private static final String PHP_NATURE = "org.eclipse.php.core.PHPNature"; //$NON-NLS-1$
 
 	public boolean test(Object receiver, String property, Object[] args,
 			Object expectedValue) {
+		boolean result = false;
 		if (receiver instanceof IAdaptable) {
 			IAdaptable adaptable = (IAdaptable) receiver;
 			Object adapter = (IResource) adaptable.getAdapter(IResource.class);
@@ -41,39 +36,15 @@ public class DeploymentTester extends PropertyTester {
 				receiver = adapter;
 			}
 		}
-		IProject project = null;
 		if (receiver instanceof IProject) {
-			project = (IProject) receiver;
+			IProject project = (IProject) receiver;
 			try {
 				Object phpNature = project.getNature(PHP_NATURE);
-				if (phpNature == null) {
-					return false;
+				if (phpNature != null) {
+					result = true;
 				}
 			} catch (CoreException e) {
 				// Ignore this exception
-			}
-		}
-		if (receiver instanceof IFile) {
-			IFile file = (IFile) receiver;
-			if (!DescriptorContainerManager.DESCRIPTOR_PATH.equals(file
-					.getName())) {
-				return false;
-			}
-			project = file.getProject();
-		}
-		boolean result = false;
-		if (project != null) {
-			IResource file = project
-					.getFile((DescriptorContainerManager.DESCRIPTOR_PATH));
-			if (ADD_SUPPORT.equals(property)) {
-				result = !file.exists();
-			} else if (DEPLOY.equals(property)) {
-				result = file.exists();
-			} else if (QUICK_DEPLOY.equals(property)) {
-				result = file.exists();
-				if (result == true) {
-					result = ServerUtils.getTarget(project) != null;
-				}
 			}
 		}
 		return result == (Boolean) expectedValue;
