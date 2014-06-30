ca1f00633d2e201180e58e5a2986e65bcc82be47
Natalia Bartol <natalia.b@zend.com>
2014-06-30 22:20:10 +0100

    Expose 'Add Server' wizard on Zend PDT Welcome page.
    Signed-off-by: Natalia Bartol <natalia.b@zend.com>

---
+++ zendserver-deployment-eclipse/org.zend.php.server.ui/src/org/zend/php/server/internal/ui/actions/LaunchServerWizard.java
@@ -0,0 +1,19 @@
+package org.zend.php.server.internal.ui.actions;
+
+import org.eclipse.ui.IWorkbench;
+import org.zend.php.server.internal.ui.ServersUI;
+
+public class LaunchServerWizard implements Runnable {
+
+	
+	public void launchWizard(IWorkbench workbench) {
+		AddServerAction action = new AddServerAction();
+		action.run();
+	}
+	
+	public void run() {
+		IWorkbench w = ServersUI.getDefault().getWorkbench();
+		launchWizard(w);
+	}
+
+}
