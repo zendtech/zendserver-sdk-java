246f98f05115f774e5e5c63a46d22c49549d03cb
Kaloyan Raev <kaloyan.r@zend.com>
2014-11-28 23:49:20 +0200

    Handle cloned editors in DescriptorReconcilingStrategy

--- zendserver-deployment-eclipse/org.zend.php.zendserver.deployment.ui/src/org/zend/php/zendserver/deployment/ui/editors/text/DescriptorReconcilingStrategy.java
+++ zendserver-deployment-eclipse/org.zend.php.zendserver.deployment.ui/src/org/zend/php/zendserver/deployment/ui/editors/text/DescriptorReconcilingStrategy.java
@@ -1,11 +1,14 @@
 package org.zend.php.zendserver.deployment.ui.editors.text;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.IEditorInput;
-import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IEditorReference;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.PlatformUI;
@@ -20,7 +23,7 @@ import org.zend.php.zendserver.deployment.core.internal.validation.ValidationSta
 import org.zend.php.zendserver.deployment.ui.editors.DeploymentDescriptorEditor;
 
 public class DescriptorReconcilingStrategy implements IValidator {
-	
+
 	private DescriptorSemanticValidator validator = new DescriptorSemanticValidator();
 
 	public DescriptorReconcilingStrategy() {
@@ -31,49 +34,36 @@ public class DescriptorReconcilingStrategy implements IValidator {
 	}
 
 	public void validate(IValidationContext helper, IReporter reporter) {
-		DeploymentDescriptorEditor editor = findEditor(helper);
-		if (editor == null)
-			return;
-		
-		IDescriptorContainer container = editor.getDescriptorContainer();
-		container.load();
-		
-		validator.setFile(container.getFile());
-		ValidationStatus[] statuses = validator.validate(container.getDescriptorModel(), editor.getDocument());
-		DescriptorValidator.reportProblems(container.getFile(), statuses);
+		List<IEditorReference> editors = findEditors(helper);
+		for (IEditorReference ref : editors) {
+			DeploymentDescriptorEditor editor = (DeploymentDescriptorEditor) ref
+					.getEditor(true);
+			IDescriptorContainer container = editor.getDescriptorContainer();
+			container.load();
+
+			validator.setFile(container.getFile());
+			ValidationStatus[] statuses = validator.validate(
+					container.getDescriptorModel(), editor.getDocument());
+			DescriptorValidator.reportProblems(container.getFile(), statuses);
+		}
 	}
 
-	private DeploymentDescriptorEditor findEditor(IValidationContext helper) {
+	private List<IEditorReference> findEditors(IValidationContext helper) {
 		String[] path = helper.getURIs();
-		if (path.length == 0) 
+		if (path.length == 0)
 			return null;
-		
-		IFile file = getFile(path[0]);
+
+		IFile file = ResourcesPlugin.getWorkspace().getRoot()
+				.getFile(new Path(path[0]));
 		if (file == null)
 			return null;
-		
-		IEditorPart editor = getEditor(new FileEditorInput(file));
-		if (editor == null)
-			return null;
-		
-		if (!(editor instanceof DeploymentDescriptorEditor))
-			return null;
-		
-		return (DeploymentDescriptorEditor) editor;
+
+		return getEditors(new FileEditorInput(file));
 	}
 
-	private IFile getFile(String path) {
-		try {
-			return ResourcesPlugin.getWorkspace().getRoot()
-					.getFile(new Path(path));
-		} catch (Exception e) {
-		}
-		return null;
-	}
-	
+	private List<IEditorReference> getEditors(final IEditorInput editorInput) {
+		final List<IEditorReference> editors = new ArrayList<IEditorReference>();
 
-	private IEditorPart getEditor(final IEditorInput editorInput) {
-		final IEditorPart editor[] = new IEditorPart[1];
 		Display.getDefault().syncExec(new Runnable() {
 			// needs UI thread to retrieve active page
 			public void run() {
@@ -84,13 +74,12 @@ public class DescriptorReconcilingStrategy implements IValidator {
 							editorInput, DeploymentDescriptorEditor.ID,
 							IWorkbenchPage.MATCH_ID
 									| IWorkbenchPage.MATCH_INPUT);
-					if (refs.length > 0) {
-						editor[0] = refs[0].getEditor(true);
-					}
+					editors.addAll(Arrays.asList(refs));
 				}
 			}
 		});
-		return editor[0];
+
+		return editors;
 	}
 
 }
