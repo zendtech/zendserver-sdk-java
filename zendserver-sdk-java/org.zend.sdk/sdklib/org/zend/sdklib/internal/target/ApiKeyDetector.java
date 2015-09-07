9e764b46a3b3d1c53c754e414b0a4d6a092e4309
Dariusz Michura <dariusz.m@zend.com>
2015-09-07 16:30:02 +0200

    ZSTD-5210 Cannot add server 8.5.1

--- zendserver-sdk-java/org.zend.sdk/sdklib/org/zend/sdklib/internal/target/ApiKeyDetector.java
+++ zendserver-sdk-java/org.zend.sdk/sdklib/org/zend/sdklib/internal/target/ApiKeyDetector.java
@@ -7,7 +7,11 @@
  *******************************************************************************/
 package org.zend.sdklib.internal.target;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.text.MessageFormat;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
@@ -71,25 +75,23 @@ public abstract class ApiKeyDetector {
 			String sessionId = login();
 			if (sessionId != null) {
 				String exisitngKeys = getApiKeys(sessionId);
-				Map<String, String> keys = ZendTargetAutoDetect
-						.parseApiKey(exisitngKeys);
+				Map<String, String> keys = ZendTargetAutoDetect.parseApiKey(exisitngKeys);
 				String keyName = name != null ? name : DEFAULT_KEY;
 				if (keys.containsKey(keyName)) {
 					name = keyName;
 					secretKey = keys.get(keyName);
 					return true;
 				}
-				String content = doCreateApiKey(sessionId);
-				Map<String, String> values = ZendTargetAutoDetect
-						.parseApiKey(content);
+				String accessToken = getAccessToken(sessionId);
+				String content = doCreateApiKey(sessionId, accessToken);
+				Map<String, String> values = ZendTargetAutoDetect.parseApiKey(content);
 				if (values.containsKey(keyName)) {
 					name = keyName;
 					secretKey = values.get(keyName);
 				}
 				return true;
 			}
-			throw new SdkException(
-					"Could not connect with a local Zend Server.");
+			throw new SdkException("Could not connect with a local Zend Server.");
 		} finally {
 			username = null;
 			password = null;
@@ -131,6 +133,50 @@ public abstract class ApiKeyDetector {
 		return password;
 	}
 
+	/**
+	 * Returns server access token. Valid for Zend Server 8.5.1 and higher.
+	 * 
+	 * @param sessionId
+	 * @return server access token or <code>null</code>.
+	 * @throws SdkException
+	 */
+	private String getAccessToken(String sessionId) throws SdkException {
+		Map<String, String> cookies = new HashMap<String, String>();
+		cookies.put(SESSION_ID, sessionId);
+
+		String accessToken = null;
+		HttpClient client = new HttpClient();
+		HttpMethodBase method = new GetMethod(serverUrl);
+		setCookies(method, cookies);
+		try {
+			int statusCode = client.executeMethod(method);
+			if(statusCode != 200){
+				String message = MessageFormat.format("Could not execute method: '{0}'. Returned status code: '{1}'",
+						method.getURI().toString(), statusCode);
+				throw new SdkException(message);
+			}
+			InputStream responseBody = method.getResponseBodyAsStream();
+			BufferedReader in = new BufferedReader(new InputStreamReader(responseBody));
+			// looking for: var csrf = '<128-lenght-combinantion-of-chars>'
+			String line = null;
+			@SuppressWarnings("unused")
+			int index = -1;
+			while ((line = in.readLine()) != null) {
+				if ((index = line.indexOf("var csrf")) == -1) //$NON-NLS-1$
+					continue;
+
+				int start = line.indexOf("'"); //$NON-NLS-1$
+				int end = line.lastIndexOf("'"); //$NON-NLS-1$
+				return line.substring(start + 1, end);
+			}
+		} catch (IOException e) {
+			throw new SdkException("Could not obtain server access token", e);
+		} finally {
+			method.releaseConnection();
+		}
+		return accessToken;
+	}
+
 	private String login() throws SdkException {
 		Map<String, String> params = new HashMap<String, String>();
 		params.put(USERNAME, username);
@@ -139,13 +185,13 @@ public abstract class ApiKeyDetector {
 		return executeLogin(url, params);
 	}
 
-	private String doCreateApiKey(String sessionId) throws SdkException {
+	private String doCreateApiKey(String sessionId, String accessToken) throws SdkException {
 		Map<String, String> params = new HashMap<String, String>();
 		Map<String, String> cookies = new HashMap<String, String>();
 		cookies.put(SESSION_ID, sessionId);
 		params.put(NAME, name != null ? name : DEFAULT_KEY);
 		params.put(USERNAME, "admin"); //$NON-NLS-1$
-		return executeAddApiKey(getUrl("/Api/apiKeysAddKey"), params, cookies); //$NON-NLS-1$
+		return executeAddApiKey(getUrl("/Api/apiKeysAddKey"), params, cookies, accessToken); //$NON-NLS-1$
 	}
 
 	private String getApiKeys(String sessionId) throws SdkException {
@@ -218,14 +264,17 @@ public abstract class ApiKeyDetector {
 		return null;
 	}
 
-	private String executeAddApiKey(String url, Map<String, String> params,
-			Map<String, String> cookies) throws SdkException {
+	private String executeAddApiKey(String url, Map<String, String> params, Map<String, String> cookies,
+			String accessToken) throws SdkException {
 		HttpClient client = new HttpClient();
 		HttpMethodBase method = createPostRequest(url, params);
 		setCookies(method, cookies);
 		method.setRequestHeader("X-Accept", //$NON-NLS-1$
 				"application/vnd.zend.serverapi+json;version=1.3;q=1.0"); //$NON-NLS-1$
 		method.setRequestHeader("X-Request", "JSON"); //$NON-NLS-1$ //$NON-NLS-2$
+		if (accessToken != null) {
+			method.setRequestHeader("Access-Token", accessToken); //$NON-NLS-1$
+		}
 		if (method != null) {
 			int statusCode = -1;
 			try {
@@ -237,7 +286,7 @@ public abstract class ApiKeyDetector {
 				} else if (statusCode == 500) {
 					String val = params.remove(NAME);
 					params.put(NAME, val + new Random().nextInt());
-					return executeAddApiKey(url, params, cookies);
+					return executeAddApiKey(url, params, cookies, accessToken);
 				}
 			} catch (IOException e) {
 				throw new SdkException(e);
